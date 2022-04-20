9e46a01c658e7a1569da73ccafd79037feb2f325
Jack Kelly <kcjackkelly@gmail.com>
2022-04-20 11:23:27 -0500

    x

---
+++ pages/api/events/[event]/activities/index.ts
@@ -0,0 +1,21 @@
+import { NextApiRequest, NextApiResponse } from 'next';
+import { getSession } from 'next-auth/react';
+
+export default async (req: NextApiRequest, res: NextApiResponse) => {
+	const session = await getSession({ req });
+	const { event } = req.query;
+
+	if (!session?.user?.id) {
+		return res.status(401).send({ message: 'You must be logged in to do this.' });
+	}
+
+	try {
+		return res.status(200).send('activities for event id: ' + event);
+	} catch (error) {
+		if (error instanceof Error) {
+			return res.status(500).send(error.message);
+		}
+
+		return res.status(500).send('An error occurred, please try again.');
+	}
+};
