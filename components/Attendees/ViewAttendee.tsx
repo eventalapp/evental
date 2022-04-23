29bc4aa212fe161b8fd6666a69aeab6ff381b3e6
Jack Kelly <kcjackkelly@gmail.com>
2022-04-22 22:56:20 -0500

    x

--- components/Attendees/ViewAttendee.tsx
+++ components/Attendees/ViewAttendee.tsx
@@ -1,4 +1,7 @@
 import { EventMemberUser } from '../../pages/api/events/[eid]/attendees/[aid]';
+import { capitalizeFirstLetter } from '../../utils/string';
+import Image from 'next/image';
+import React from 'react';
 
 interface Props {
 	loading: boolean;
@@ -26,11 +29,19 @@ export const ViewAttendee: React.FC<Props> = (props) => {
 
 	return (
 		<div>
-			{attendee && attendee.user && (
+			{attendee && attendee.user && attendee.role && (
 				<div>
-					<img alt={String(attendee.user.name)} src={String(attendee.user.image)} />
+					<div className="h-32 w-32 relative">
+						<Image
+							alt={String(attendee.user.name)}
+							src={String(attendee.user.image)}
+							className="rounded-full"
+							layout="fill"
+						/>
+					</div>
 					<h1 className="text-3xl">{attendee.user.name}</h1>
-					<p>{attendee.permissionRole}</p>
+					<p>{capitalizeFirstLetter(String(attendee.permissionRole).toLowerCase())}</p>
+					<p>{capitalizeFirstLetter(String(attendee.role.name).toLowerCase())}</p>
 					<span className="text-md text-gray-700 block">{attendee.user.company}</span>
 					<span className="text-md text-gray-700 block">{attendee.user.position}</span>
 				</div>
