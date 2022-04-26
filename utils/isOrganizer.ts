1029cca53df5129234e720a0fb06d73aff8ce116
Jack Kelly <kcjackkelly@gmail.com>
2022-04-25 21:17:44 -0500

    await isOrganizer

--- utils/isOrganizer.ts
+++ utils/isOrganizer.ts
@@ -1,15 +1,29 @@
 import prisma from '../prisma/client';
 
 export const isOrganizer = async (userId: string, eventId: string) => {
-	return Boolean(
-		await prisma.eventAttendee.findFirst({
-			where: {
-				userId: userId,
-				event: {
-					OR: [{ id: eventId }, { slug: eventId }]
-				},
-				OR: [{ permissionRole: 'FOUNDER' }, { permissionRole: 'ORGANIZER' }]
-			}
-		})
-	);
+	const event = await prisma.event.findFirst({
+		where: {
+			OR: [{ id: String(eventId) }, { slug: String(eventId) }]
+		}
+	});
+
+	if (!event) {
+		return false;
+	}
+
+	const attendee = await prisma.eventAttendee.findFirst({
+		where: {
+			userId: userId,
+			event: {
+				id: event.id
+			},
+			OR: [{ permissionRole: 'FOUNDER' }, { permissionRole: 'ORGANIZER' }]
+		}
+	});
+
+	if (!attendee) {
+		return false;
+	}
+
+	return Boolean();
 };
