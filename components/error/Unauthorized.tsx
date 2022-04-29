d455f576ce66bd24a81ec0c5b9340aca5361c080
Jack Kelly <kcjackkelly@gmail.com>
2022-04-29 11:59:44 -0500

    fix unauthorized page

--- components/error/Unauthorized.tsx
+++ components/error/Unauthorized.tsx
@@ -3,28 +3,18 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import Link from 'next/link';
 import React from 'react';
 import { LinkButton } from '../form/LinkButton';
-import Column from '../layout/Column';
-import { Navigation } from '../navigation';
 
 const Unauthorized: React.FC = () => {
 	return (
 		<div>
-			<Navigation />
-
-			<Column>
-				<h1 className="text-3xl mb-2 font-bold">Unauthorized</h1>
-				<span className="mb-5 block">You must sign in to view this page.</span>
-				<Link href="/auth/signin" passHref>
-					<LinkButton variant="gradient" padding="large">
-						Sign in
-						<FontAwesomeIcon
-							className="cursor-pointer ml-2"
-							size="1x"
-							icon={faArrowRightToBracket}
-						/>
-					</LinkButton>
-				</Link>
-			</Column>
+			<h1 className="text-3xl mb-2 font-bold">Unauthorized</h1>
+			<span className="mb-5 block">You must sign in to view this page.</span>
+			<Link href="/auth/signin" passHref>
+				<LinkButton variant="gradient" padding="large">
+					Sign in
+					<FontAwesomeIcon className="cursor-pointer ml-2" size="1x" icon={faArrowRightToBracket} />
+				</LinkButton>
+			</Link>
 		</div>
 	);
 };
