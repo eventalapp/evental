7b8e75eb7d840d1012356884f04fc4f2e1727fa0
Jack Kelly <kcjackkelly@gmail.com>
2022-04-29 15:26:55 -0500

    add icon for role name

--- components/attendees/ViewAttendee.tsx
+++ components/attendees/ViewAttendee.tsx
@@ -7,7 +7,12 @@ import { LinkButton } from '../form/LinkButton';
 import { UseOrganizerQueryData } from '../../hooks/queries/useOrganizerQuery';
 import { FlexRowBetween } from '../layout/FlexRowBetween';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faAddressBook, faBuilding, faLocationDot } from '@fortawesome/free-solid-svg-icons';
+import {
+	faAddressBook,
+	faAddressCard,
+	faBuilding,
+	faLocationDot
+} from '@fortawesome/free-solid-svg-icons';
 
 type Props = { eid: string; aid: string } & UseAttendeeQueryData & UseOrganizerQueryData;
 
@@ -45,7 +50,17 @@ export const ViewAttendee: React.FC<Props> = (props) => {
 				)}
 			</FlexRowBetween>
 			<h1 className="text-3xl font-bold">{attendee.name}</h1>
-			<p>{capitalizeFirstLetter(String(attendee.role.name))}</p>
+
+			<div className="flex flex-row items-center mb-1">
+				<FontAwesomeIcon
+					fill="currentColor"
+					className="w-5 h-5 mr-1.5"
+					size="1x"
+					icon={faAddressCard}
+				/>
+				<p>{capitalizeFirstLetter(String(attendee.role.name))}</p>
+			</div>
+
 			<div className="flex flex-row items-center mb-1">
 				<FontAwesomeIcon
 					fill="currentColor"
