e4fc98ae0b70490ec58b25c531f2f1695e91aa6c
Jack Kelly <kcjackkelly@gmail.com>
2022-04-28 23:07:32 -0500

    SRR event view

--- pages/events/[eid]/index.tsx
+++ pages/events/[eid]/index.tsx
@@ -1,4 +1,4 @@
-import type { NextPage } from 'next';
+import type { GetServerSideProps, NextPage } from 'next';
 import Head from 'next/head';
 import { useRouter } from 'next/router';
 import Column from '../../../components/layout/Column';
@@ -9,20 +9,50 @@ import { useEventQuery } from '../../../hooks/queries/useEventQuery';
 import { useOrganizerQuery } from '../../../hooks/queries/useOrganizerQuery';
 import { useRolesQuery } from '../../../hooks/queries/useRolesQuery';
 import { useAttendeeByUserIdQuery } from '../../../hooks/queries/useAttendeeByUserIdQuery';
-import { useSession } from 'next-auth/react';
+import { getSession } from 'next-auth/react';
 import PageWrapper from '../../../components/layout/PageWrapper';
+import Prisma from '@prisma/client';
+import { getEvent } from '../../api/events/[eid]';
+import { getIsOrganizer } from '../../api/events/[eid]/organizer';
+import { getAttendeeByUserId } from '../../api/events/[eid]/attendees/user/[uid]';
+import { getActivities } from '../../api/events/[eid]/activities';
+import { EventAttendeeUser } from '../../api/events/[eid]/attendees/[aid]';
+import { getRoles } from '../../api/events/[eid]/roles';
+import { Session } from 'next-auth';
 
-const ViewEventPage: NextPage = () => {
+type Props = {
+	initialEvent: Prisma.Event | undefined;
+	initialActivities: Prisma.EventActivity[] | undefined;
+	initialRoles: Prisma.EventRole[] | undefined;
+	initialIsAttendeeByUserId: EventAttendeeUser | undefined;
+	initialOrganizer: boolean;
+	session: Session | null;
+};
+
+const ViewEventPage: NextPage<Props> = (props) => {
+	const {
+		initialEvent,
+		initialOrganizer,
+		session,
+		initialRoles,
+		initialActivities,
+		initialIsAttendeeByUserId
+	} = props;
 	const router = useRouter();
-	const { data: session } = useSession();
 
 	const { eid } = router.query;
-	const { isOrganizer, isOrganizerLoading, isOrganizerError } = useOrganizerQuery(String(eid));
-	const { event, isEventLoading, eventError } = useEventQuery(String(eid));
-	const { activities, isActivitiesLoading, activitiesError } = useActivitiesQuery(String(eid));
-	const { roles, isRolesLoading, rolesError } = useRolesQuery(String(eid));
+	const { isOrganizer, isOrganizerLoading, isOrganizerError } = useOrganizerQuery(
+		String(eid),
+		initialOrganizer
+	);
+	const { event, isEventLoading, eventError } = useEventQuery(String(eid), initialEvent);
+	const { activities, isActivitiesLoading, activitiesError } = useActivitiesQuery(
+		String(eid),
+		initialActivities
+	);
+	const { roles, isRolesLoading, rolesError } = useRolesQuery(String(eid), initialRoles);
 	const { attendeeByUserId, attendeeByUserIdError, isAttendeeByUserIdLoading } =
-		useAttendeeByUserIdQuery(String(eid), String(session?.user?.id));
+		useAttendeeByUserIdQuery(String(eid), String(session?.user?.id), initialIsAttendeeByUserId);
 
 	return (
 		<PageWrapper variant="gray">
@@ -54,6 +84,29 @@ const ViewEventPage: NextPage = () => {
 			</Column>
 		</PageWrapper>
 	);
+};
+
+export const getServerSideProps: GetServerSideProps<Props> = async (context) => {
+	const { eid } = context.query;
+
+	const session = await getSession(context);
+	const initialEvent = (await getEvent(String(eid))) ?? undefined;
+	const initialActivities = (await getActivities(String(eid))) ?? undefined;
+	const initialRoles = (await getRoles(String(eid))) ?? undefined;
+	const initialOrganizer = await getIsOrganizer(session?.user.id, String(eid));
+	const initialIsAttendeeByUserId =
+		(await getAttendeeByUserId(String(eid), String(session?.user?.id))) ?? undefined;
+
+	return {
+		props: {
+			initialEvent,
+			session,
+			initialOrganizer,
+			initialIsAttendeeByUserId,
+			initialRoles,
+			initialActivities
+		}
+	};
 };
 
 export default ViewEventPage;
