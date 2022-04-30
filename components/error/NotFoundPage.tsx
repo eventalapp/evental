d57939b0229e3f2124dec5931ad2e03a856acfde
Jack Kelly <kcjackkelly@gmail.com>
2022-04-29 19:37:19 -0500

    not found component refactor

--- components/error/NotFoundPage.tsx
+++ components/error/NotFoundPage.tsx
@@ -4,6 +4,8 @@ import Column from '../layout/Column';
 import { NotFound } from './NotFound';
 import PageWrapper from '../layout/PageWrapper';
 import React from 'react';
+import Link from 'next/link';
+import { LinkButton } from '../form/LinkButton';
 
 export const NotFoundPage: React.FC<{ message?: string }> = (props) => {
 	const { message = 'Not found.' } = props;
@@ -17,7 +19,11 @@ export const NotFoundPage: React.FC<{ message?: string }> = (props) => {
 			<Navigation />
 
 			<Column>
+				<h1 className="text-3xl font-bold">Not Found</h1>
 				<NotFound message={message} />
+				<Link href="/events">
+					<LinkButton>Go to events</LinkButton>
+				</Link>
 			</Column>
 		</PageWrapper>
 	);
