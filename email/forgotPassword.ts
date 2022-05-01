5ec304cc0a1d40c80fca22bfb4c650cd75c39e2d
Jack Kelly <kcjackkelly@gmail.com>
2022-05-01 14:09:45 -0500

    mjml basic impl

---
+++ email/forgotPassword.ts
@@ -0,0 +1,17 @@
+import mjml2html from 'mjml';
+
+const htmlOutput = mjml2html(`
+  <mjml>
+    <mj-body>
+      <mj-section>
+        <mj-column>
+          <mj-text>
+            Hello World!
+          </mj-text>
+        </mj-column>
+      </mj-section>
+    </mj-body>
+  </mjml>
+`);
+
+console.log(htmlOutput);
