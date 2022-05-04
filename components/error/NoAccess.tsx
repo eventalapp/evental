70db8a32e7c62c88011f5f29ffb9da9bb8c3de0f
Jack Kelly <kcjackkelly@gmail.com>
2022-05-04 13:28:36 -0500

    no access page

--- components/error/NoAccess.tsx
+++ components/error/NoAccess.tsx
@@ -6,11 +6,11 @@ const NoAccess: React.FC = () => {
 	return (
 		<>
 			<h1 className="text-2xl md:text-3xl mt-4 font-bold text-center sm:text-4xl font-display">
-				Unauthorized
+				No Access
 			</h1>
-			<p className="mt-3">You do not have access to view this page</p>
-			<Link href="/auth/signin" passHref>
-				<LinkButton>Sign in</LinkButton>
+			<p className="my-3">You do not have access to view this page</p>
+			<Link href="/events/" passHref>
+				<LinkButton>Return to events</LinkButton>
 			</Link>
 		</>
 	);
