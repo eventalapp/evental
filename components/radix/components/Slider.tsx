47bf8c52faf36487191563ef2163ddb5b6711216
Jack Kelly <kcjackkelly@gmail.com>
2022-05-04 19:08:41 -0500

    slider component

--- components/radix/components/Slider.tsx
+++ components/radix/components/Slider.tsx
@@ -2,23 +2,37 @@ import * as SliderPrimitive from '@radix-ui/react-slider';
 import cx from 'classnames';
 import React from 'react';
 
-interface Props {}
+interface Props {
+	defaultValue?: number;
+	max?: number;
+	min?: number;
+	step?: number;
+	onChange?: (value: number) => void;
+	value?: number;
+}
 
 const Slider = (props: Props) => {
+	const { defaultValue = 50, max = 100, min = 0, step = 1, value, onChange } = props;
+
 	return (
 		<SliderPrimitive.Root
-			defaultValue={[50]}
-			max={100}
-			step={1}
+			defaultValue={[defaultValue]}
+			max={max}
+			step={step}
+			min={min}
+			value={[value] as number[]}
+			onValueChange={(value) => {
+				onChange?.(value[0]);
+			}}
 			aria-label="value"
-			className="relative flex h-5 w-64 touch-none items-center"
+			className="relative flex h-5 w-96 touch-none items-center"
 		>
-			<SliderPrimitive.Track className="relative h-1 w-full grow rounded-full bg-white dark:bg-gray-800">
+			<SliderPrimitive.Track className="relative h-1 w-full grow rounded-full bg-gray-200 dark:bg-gray-800">
 				<SliderPrimitive.Range className="absolute h-full rounded-full bg-primary-600 dark:bg-white" />
 			</SliderPrimitive.Track>
 			<SliderPrimitive.Thumb
 				className={cx(
-					'block h-5 w-5 rounded-full bg-primary-600 dark:bg-white',
+					'block h-5 w-5 rounded-full bg-primary-500 dark:bg-white',
 					'focus:outline-none focus-visible:ring focus-visible:ring-primary-500 focus-visible:ring-opacity-75'
 				)}
 			/>
