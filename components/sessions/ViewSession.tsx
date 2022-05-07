a7f9953c4b2f80ae2d712924e1c819079bc4799c
Jack Kelly <kcjackkelly@gmail.com>
2022-05-06 19:50:58 -0500

    remove cursor help on clickable links

--- components/sessions/ViewSession.tsx
+++ components/sessions/ViewSession.tsx
@@ -61,7 +61,7 @@ export const ViewSession: React.FC<Props> = (props) => {
 					<Link href={`/events/${eid}/sessions/types/${session?.type?.slug}`}>
 						<a>
 							<Tooltip message={`This session is a ${session?.type?.name} session.`}>
-								<div className="inline-flex flex-row items-center mb-1 cursor-help">
+								<div className="inline-flex flex-row items-center mb-1">
 									<FontAwesomeIcon
 										fill="currentColor"
 										className="w-5 h-5 mr-1.5"
@@ -83,7 +83,7 @@ export const ViewSession: React.FC<Props> = (props) => {
 							<Tooltip
 								message={`This session is taking place at the ${session?.venue?.name} venue.`}
 							>
-								<div className="inline-flex flex-row items-center mb-1 cursor-help">
+								<div className="inline-flex flex-row items-center mb-1">
 									<FontAwesomeIcon
 										fill="currentColor"
 										className="w-5 h-5 mr-1.5"
