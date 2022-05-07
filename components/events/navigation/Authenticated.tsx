1b1f77b1a0fcfada98920fc2ccc3e456e49662d7
Jack Kelly <kcjackkelly@gmail.com>
2022-05-07 09:17:15 -0500

    remove unused support link

--- components/events/navigation/Authenticated.tsx
+++ components/events/navigation/Authenticated.tsx
@@ -135,48 +135,6 @@ export const Authenticated: React.FC<Props> = (props) => {
 										label={`${capitalizeFirstLetter(role.name.toLowerCase())}s`}
 									/>
 								))}
-
-								<NavigationMenuPrimitive.Item className="pb-1.5">
-									<NavigationMenuPrimitive.Trigger
-										className={cx(
-											'px-3 py-1.5 text-sm rounded-md hover:bg-gray-75 dark:hover:bg-gray-900',
-											'text-sm font-medium text-gray-900 dark:text-gray-75 font-medium',
-											'focus:outline-none focus-visible:ring focus-visible:ring-primary-500 focus-visible:ring-opacity-75'
-										)}
-									>
-										Resources
-									</NavigationMenuPrimitive.Trigger>
-
-									<NavigationMenuPrimitive.Content
-										className={cx(
-											'absolute w-auto top-0 left-0 rounded-lg bg-white border border-gray-200',
-											'radix-motion-from-start:animate-enter-from-left',
-											'radix-motion-from-end:animate-enter-from-right',
-											'radix-motion-to-start:animate-exit-to-left',
-											'radix-motion-to-end:animate-exit-to-right'
-										)}
-									>
-										<div className="w-[16rem] lg:w-[18rem] p-3">
-											<div className="w-full flex flex-col space-y-2">
-												<NavigationMenuPrimitive.Link
-													className={cx(
-														'w-full px-4 py-3 hover:bg-gray-75 dark:hover:bg-gray-900 rounded-md',
-														'focus:outline-none focus-visible:ring focus-visible:ring-primary-500 focus-visible:ring-opacity-75'
-													)}
-													href="mailto:support@evental.app"
-												>
-													<span className="text-sm font-medium text-gray-900 dark:text-gray-75">
-														Support
-													</span>
-
-													<div className="mt-1 text-sm text-gray-900 dark:text-gray-400">
-														Email us at support@evental.app with any questions or concerns.
-													</div>
-												</NavigationMenuPrimitive.Link>
-											</div>
-										</div>
-									</NavigationMenuPrimitive.Content>
-								</NavigationMenuPrimitive.Item>
 							</div>
 						</div>
 
