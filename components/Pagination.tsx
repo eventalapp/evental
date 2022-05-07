a5e9b755ff4f0342c690cf48628c1f630067c4b7
Jack Kelly <kcjackkelly@gmail.com>
2022-05-07 16:17:20 -0500

    pagination component

---
+++ components/Pagination.tsx
@@ -0,0 +1,46 @@
+import { scrollTo } from '../utils/scroll';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faChevronLeft, faChevronRight } from '@fortawesome/free-solid-svg-icons';
+import React from 'react';
+
+type Props = {
+	page: number;
+	pageCount: number;
+	setPage: React.Dispatch<React.SetStateAction<number>>;
+};
+
+export const Pagination: React.FC<Props> = (props) => {
+	const { page, pageCount, setPage } = props;
+
+	return (
+		<div className="flex flex-row justify-end items-center">
+			<button
+				onClick={() => {
+					if (page > 1) {
+						scrollTo(0);
+						setPage((oldPage) => oldPage - 1);
+					}
+				}}
+			>
+				<FontAwesomeIcon className="mr-1.5" fill="currentColor" icon={faChevronLeft} />
+				Prev
+			</button>
+			<span className="mx-4 text-lg font-medium">
+				Page {page}/{pageCount}
+			</span>
+			<button
+				disabled={!(page < pageCount)}
+				className="disabled:text-gray-300 disabled:cursor-not-allowed"
+				onClick={() => {
+					if (page < pageCount) {
+						scrollTo(0);
+						setPage((oldPage) => oldPage + 1);
+					}
+				}}
+			>
+				Next
+				<FontAwesomeIcon className="ml-1.5" fill="currentColor" icon={faChevronRight} />
+			</button>
+		</div>
+	);
+};
