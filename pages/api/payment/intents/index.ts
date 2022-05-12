3edf226021129181d9c36397b38cc8abc1cf3b52
Jack Kelly <kcjackkelly@gmail.com>
2022-05-12 13:34:25 -0500

    nextkit error

--- pages/api/payment/intents/index.ts
+++ pages/api/payment/intents/index.ts
@@ -2,6 +2,7 @@ import Stripe from 'stripe';
 import { CURRENCY, MAX_AMOUNT, MIN_AMOUNT } from '../../../../config';
 import { formatAmountForStripe } from '../../../../utils/stripeHelpers';
 import { api } from '../../../../utils/api';
+import { NextkitError } from 'nextkit';
 
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
 	apiVersion: '2020-08-27'
@@ -11,11 +12,10 @@ export default api({
 	async POST({ ctx, req }) {
 		const { amount, metadata }: { amount: number; metadata: Record<string, string> } = req.body;
 
-		// Validate the amount that was passed from the client.
 		if (!(amount >= MIN_AMOUNT && amount <= MAX_AMOUNT)) {
-			throw new Error('Invalid amount.');
+			throw new NextkitError(400, 'Invalid amount.');
 		}
-		// Create PaymentIntent from body params.
+
 		const params: Stripe.PaymentIntentCreateParams = {
 			payment_method_types: ['card'],
 			amount: formatAmountForStripe(amount, CURRENCY),
