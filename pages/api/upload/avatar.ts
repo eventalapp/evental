a62cbac5b883d853c5f1a5d9f81733db5d030afa
Jack Kelly <kcjackkelly@gmail.com>
2022-05-12 22:41:55 -0500

    avatar upload

---
+++ pages/api/upload/avatar.ts
@@ -0,0 +1,38 @@
+import { busboyParseForm } from '../../../utils/busboyParseForm';
+import { api } from '../../../utils/api';
+import { NextkitError } from 'nextkit';
+import { uploadAndProcessAvatar } from '../../../utils/uploadAndProcessImage';
+
+export const config = {
+	api: {
+		bodyParser: false
+	}
+};
+
+export type ImageUploadResponse = {
+	pathName?: string;
+};
+
+export default api({
+	async POST({ ctx, req }) {
+		const user = await ctx.getUser();
+
+		if (!user?.id) {
+			throw new NextkitError(401, 'You must be logged in to do this.');
+		}
+
+		const { buffer, mimeType } = await busboyParseForm(req);
+
+		let fileLocation = await uploadAndProcessAvatar(buffer, mimeType);
+
+		if (!fileLocation && buffer.length >= 1) {
+			throw new NextkitError(500, 'Image failed to upload.');
+		}
+
+		const body: ImageUploadResponse = {
+			pathName: fileLocation
+		};
+
+		return body;
+	}
+});
