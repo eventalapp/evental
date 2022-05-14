4d6f91585f079eaacd5fbe7ddeef8b997a7afd83
Jack Kelly <kcjackkelly@gmail.com>
2022-05-14 18:26:35 -0500

    wrap pro purchase form in elements provider

--- pages/events/[eid]/admin/billing/index.tsx
+++ pages/events/[eid]/admin/billing/index.tsx
@@ -14,11 +14,9 @@ import { useUser } from '../../../../../hooks/queries/useUser';
 import { UnauthorizedPage } from '../../../../../components/error/UnauthorizedPage';
 import { LoadingPage } from '../../../../../components/error/LoadingPage';
 import { useFounderQuery } from '../../../../../hooks/queries/useFounderQuery';
-import Slider from '../../../../../components/radix/components/Slider';
-import { Controller, useForm } from 'react-hook-form';
-import Link from 'next/link';
-import { LinkButton } from '../../../../../components/form/LinkButton';
-import { attendeesToPrice } from '../../../../../utils/price';
+import { Elements } from '@stripe/react-stripe-js';
+import { getStripe } from '../../../../../utils/stripe';
+import { PurchaseProPlan } from '../../../../../components/billing/PurchaseProForm';
 
 const EventBillingPage: NextPage = () => {
 	const router = useRouter();
@@ -28,9 +26,6 @@ const EventBillingPage: NextPage = () => {
 	const { isFounderLoading, isFounder } = useFounderQuery(String(eid));
 	const { roles, isRolesLoading } = useRolesQuery(String(eid));
 
-	const { control, watch } = useForm({ defaultValues: { attendees: 250 } });
-
-	const attendees = watch('attendees');
 	if (isEventLoading || isUserLoading || isFounderLoading || isRolesLoading) {
 		return <LoadingPage />;
 	}
@@ -52,83 +47,20 @@ const EventBillingPage: NextPage = () => {
 	}
 
 	return (
-		<PageWrapper variant="gray">
-			<Head>
-				<title>Event Billing</title>
-			</Head>
+		<Elements stripe={getStripe()}>
+			<PageWrapper variant="gray">
+				<Head>
+					<title>Event Billing</title>
+				</Head>
 
-			<EventSettingsNavigation event={event} roles={roles} user={user} />
+				<EventSettingsNavigation event={event} roles={roles} user={user} />
 
-			<Column className="flex flex-col items-center">
-				<h3 className="text-xl md:text-2xl font-medium">Single Event Plans & Pricing</h3>
-
-				<div className="flex flex-col items-center">
-					<div className="flex flex-col items-center space-y-3 my-8">
-						<p className="text-lg">How many attendees do you expect?</p>
-						<Controller
-							control={control}
-							name="attendees"
-							render={({ field }) => (
-								<Slider
-									onChange={(val) => {
-										field.onChange(val);
-									}}
-									value={field.value}
-									max={5000}
-									min={250}
-									step={250}
-								/>
-							)}
-						/>
-						{attendees >= 5000 && (
-							<p className="text-gray-600">
-								More than 5,000 people?{' '}
-								<Link href="/contact">
-									<a className="text-primary font-medium">Ask us for a same-day quote.</a>
-								</Link>
-							</p>
-						)}
-
-						<p className="text-xl">
-							<strong>{attendees}</strong> Attendees
-						</p>
-					</div>
-					<div className="bg-white border-gray-300 border rounded shadow-sm p-5 flex flex-col justify-between items-center space-y-4 max-w-[450px] min-h-[350px]">
-						<div className="flex flex-row items-center">
-							<strong
-								className="text-2xl tracking-tight font-bold font-display mr-2"
-								aria-label="evental homepage"
-							>
-								Evental
-							</strong>
-							<span className="bg-primary text-white px-2 py-1 font-medium text-xs rounded">
-								PRO
-							</span>
-						</div>
-
-						<p className="text-gray-700">
-							The pro plan allows event organizers to create unlimited events, sessions, venues, and
-							pages. It also allows you to invite additional organizers to help you manage your
-							event.
-						</p>
-
-						<div>
-							<p className="font-bold text-xl text-center">${attendeesToPrice(attendees)}</p>
-							<p className="text-gray-600 text-sm text-center">Includes {attendees} attendees</p>
-						</div>
-
-						<Link href={`/payment/checkout`}>
-							<LinkButton>Purchase</LinkButton>
-						</Link>
-						<Link href="/contact">
-							<a className="text-sm text-gray-500">
-								Need help? <span className="text-gray-800 underline">Contact us</span>
-							</a>
-						</Link>
-					</div>
-				</div>
-			</Column>
-		</PageWrapper>
+				<Column className="flex flex-col items-center">
+					<h3 className="text-xl md:text-2xl font-medium">Single Event Plans & Pricing</h3>
+					<PurchaseProPlan eid={String(eid)} />
+				</Column>
+			</PageWrapper>
+		</Elements>
 	);
 };
 
