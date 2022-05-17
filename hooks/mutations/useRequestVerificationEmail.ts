edc1b0db84629e9895b74c004c6a8633c99ac6e1
Jack Kelly <kcjackkelly@gmail.com>
2022-05-17 12:37:15 -0500

    useRequestVerificationEmail.ts

---
+++ hooks/mutations/useRequestVerificationEmail.ts
@@ -0,0 +1,36 @@
+import axios, { AxiosError } from 'axios';
+import { useMutation, UseMutationResult } from 'react-query';
+import { toast } from 'react-toastify';
+import { ErroredAPIResponse, SuccessAPIResponse } from 'nextkit';
+
+export interface UseRequestVerificationEmailData {
+	requestVerificationEmailMutation: UseMutationResult<
+		void,
+		AxiosError<ErroredAPIResponse, unknown>,
+		void
+	>;
+}
+
+export const useRequestVerificationEmail = (): UseRequestVerificationEmailData => {
+	const requestVerificationEmailMutation = useMutation<
+		void,
+		AxiosError<ErroredAPIResponse, unknown>,
+		void
+	>(
+		async (data) => {
+			return await axios
+				.post<SuccessAPIResponse<void>>(`/api/auth/verify/request`, data)
+				.then((res) => res.data.data);
+		},
+		{
+			onSuccess: () => {
+				toast.success('Verification request successfully sent. Check your email.');
+			},
+			onError: (error) => {
+				toast.error(error?.response?.data.message ?? 'An error has occurred.');
+			}
+		}
+	);
+
+	return { requestVerificationEmailMutation };
+};
