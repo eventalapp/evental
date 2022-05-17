01af04a6c3994cb1dcbab5c46f3ced6ecc729a29
Jack Kelly <kcjackkelly@gmail.com>
2022-05-17 12:37:29 -0500

    Your account is not verified

--- pages/settings.tsx
+++ pages/settings.tsx
@@ -11,6 +11,8 @@ import { ssrGetUser } from '../utils/api';
 import { PasswordlessUser } from '../utils/stripUserPassword';
 import { LoadingPage } from '../components/error/LoadingPage';
 import { NotFoundPage } from '../components/error/NotFoundPage';
+import React from 'react';
+import { useRequestVerificationEmail } from '../hooks/mutations/useRequestVerificationEmail';
 
 type Props = {
 	initialUser: PasswordlessUser | undefined;
@@ -20,6 +22,7 @@ const SettingsPage: NextPage<Props> = (props) => {
 	const { initialUser } = props;
 	const { user, isUserLoading } = useUser(initialUser);
 	const { editUserMutation } = useEditUserMutation(String(user?.id));
+	const { requestVerificationEmailMutation } = useRequestVerificationEmail();
 
 	if (isUserLoading) {
 		return <LoadingPage />;
@@ -38,6 +41,17 @@ const SettingsPage: NextPage<Props> = (props) => {
 			<Navigation />
 
 			<Column variant="halfWidth">
+				{!user.emailVerified && (
+					<button
+						className="block w-full bg-gradient-to-r from-primary-500 to-secondary-500 block text-white px-5 py-3 rounded-md mb-4 font-medium"
+						onClick={() => {
+							requestVerificationEmailMutation.mutate();
+						}}
+					>
+						Your account is not verified. Click here to request a verification email.
+					</button>
+				)}
+
 				<h1 className="text-2xl md:text-3xl font-bold">User Settings</h1>
 
 				<UserSettingsForm
