c668e4ec3ffd23f1529b4ea42d47417d16dbeeda
Jack Kelly <kcjackkelly@gmail.com>
2022-05-17 15:36:03 -0500

    move links to default seo

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -32,6 +32,69 @@ const App: React.FC<AppProps & { error?: Error }> = (props) => {
 		<QueryClientProvider client={queryClient}>
 			<Hydrate state={pageProps.dehydratedState}>
 				<DefaultSeo
+					additionalLinkTags={[
+						{
+							rel: 'preconnect',
+							href: 'https://fonts.googleapis.com'
+						},
+						{
+							rel: 'preconnect',
+							href: 'https://fonts.gstatic.com'
+						},
+						{
+							href: 'https://fonts.googleapis.com/css2?family=Inter:wght@400;500;700&display=swap',
+							rel: 'stylesheet'
+						},
+						{
+							rel: 'shortcut icon',
+							href: '/images/favicon.ico',
+							type: 'image/x-icon'
+						},
+						{
+							rel: 'apple-touch-icon',
+							href: '/images/apple-touch-icon.png'
+						},
+						{
+							rel: 'apple-touch-icon',
+							sizes: '57x57',
+							href: '/images/apple-touch-icon-57x57.png'
+						},
+						{
+							rel: 'apple-touch-icon',
+							sizes: '72x72',
+							href: '/images/apple-touch-icon-72x72.png'
+						},
+						{
+							rel: 'apple-touch-icon',
+							sizes: '76x76',
+							href: '/images/apple-touch-icon-76x76.png'
+						},
+						{
+							rel: 'apple-touch-icon',
+							sizes: '114x114',
+							href: '/images/apple-touch-icon-114x114.png'
+						},
+						{
+							rel: 'apple-touch-icon',
+							sizes: '120x120',
+							href: '/images/apple-touch-icon-120x120.png'
+						},
+						{
+							rel: 'apple-touch-icon',
+							sizes: '144x144',
+							href: '/images/apple-touch-icon-144x144.png'
+						},
+						{
+							rel: 'apple-touch-icon',
+							sizes: '152x152',
+							href: '/images/apple-touch-icon-152x152.png'
+						},
+						{
+							rel: 'apple-touch-icon',
+							sizes: '180x180',
+							href: '/images/apple-touch-icon-180x180.png'
+						}
+					]}
 					openGraph={{
 						type: 'website',
 						locale: 'en_IE',
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -16,45 +16,7 @@ export default class Document extends NextDocument {
 				itemType="https://schema.org/WebPage"
 				className="h-full"
 			>
-				<Head>
-					<link rel="preconnect" href="https://fonts.googleapis.com" />
-					<link rel="preconnect" href="https://fonts.gstatic.com" />
-					<link
-						href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;700&display=swap"
-						rel="stylesheet"
-					/>
-
-					<link rel="shortcut icon" href="/images/favicon.ico" type="image/x-icon" />
-					<link rel="apple-touch-icon" href="/images/apple-touch-icon.png" />
-					<link rel="apple-touch-icon" sizes="57x57" href="/images/apple-touch-icon-57x57.png" />
-					<link rel="apple-touch-icon" sizes="72x72" href="/images/apple-touch-icon-72x72.png" />
-					<link rel="apple-touch-icon" sizes="76x76" href="/images/apple-touch-icon-76x76.png" />
-					<link
-						rel="apple-touch-icon"
-						sizes="114x114"
-						href="/images/apple-touch-icon-114x114.png"
-					/>
-					<link
-						rel="apple-touch-icon"
-						sizes="120x120"
-						href="/images/apple-touch-icon-120x120.png"
-					/>
-					<link
-						rel="apple-touch-icon"
-						sizes="144x144"
-						href="/images/apple-touch-icon-144x144.png"
-					/>
-					<link
-						rel="apple-touch-icon"
-						sizes="152x152"
-						href="/images/apple-touch-icon-152x152.png"
-					/>
-					<link
-						rel="apple-touch-icon"
-						sizes="180x180"
-						href="/images/apple-touch-icon-180x180.png"
-					/>
-				</Head>
+				<Head />
 				<body className="h-full text-gray-900">
 					<Main />
 					<NextScript />
