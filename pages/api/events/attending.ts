def47490fcc17f364980336d52a4278bffa27c40
Jack Kelly <kcjackkelly@gmail.com>
2022-05-17 15:15:01 -0500

    only fetch events attending that haven't ended

--- pages/api/events/attending.ts
+++ pages/api/events/attending.ts
@@ -23,6 +23,9 @@ export const getAttendingEvents = async (uid: string): Promise<Prisma.Event[]> =
 			}
 		],
 		where: {
+			endDate: {
+				gte: new Date()
+			},
 			attendees: {
 				some: {
 					userId: uid,
