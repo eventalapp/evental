26529e013633a64e9d538adc8a5b200fec888909
Jack Kelly <kcjackkelly@gmail.com>
2022-05-18 14:22:50 -0500

    private event notice

--- components/error/PrivatePage.tsx
+++ components/error/PrivatePage.tsx
@@ -15,8 +15,12 @@ export const PrivatePage = () => (
 		<Navigation />
 
 		<Column variant="halfWidth" className="flex flex-col items-center justify-center">
-			<h1 className="text-2xl md:text-3xl mb-2 font-bold">Private</h1>
-			<span className="mb-5 block">This event is private.</span>
+			<h1 className="text-2xl md:text-3xl mb-2 font-bold">Private Event</h1>
+			<span className="mb-2 block">This event is private. Please check back later</span>
+			<span className="mb-5 block text-gray-500 text-center">
+				If you believe you should be able to see this page, please contact the event owner and ask
+				them make you an organizer.
+			</span>
 			<Link href={`/events`} passHref>
 				<LinkButton padding="large">View Events</LinkButton>
 			</Link>
