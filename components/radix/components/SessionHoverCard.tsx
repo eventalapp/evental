1d5a2f43e05d0c8c104f6f817a01e646c2d00941
Jack Kelly <kcjackkelly@gmail.com>
2022-05-22 00:58:46 -0500

    session hover card ui updates

--- components/radix/components/SessionHoverCard.tsx
+++ components/radix/components/SessionHoverCard.tsx
@@ -34,61 +34,61 @@ export const SessionHoverCard: React.FC<Props> = (props) => {
 					'radix-side-top:animate-slide-up radix-side-bottom:animate-slide-down',
 					'max-w-md rounded-lg p-4 md:w-full',
 					'bg-white dark:bg-gray-800 border-gray-200 border shadow-sm',
-					'focus:outline-none focus-visible:ring focus-visible:ring-primary-500 focus-visible:ring-opacity-75 min-w-[250px]'
+					'focus:outline-none focus-visible:ring focus-visible:ring-primary-500 focus-visible:ring-opacity-75 min-w-[300px]'
 				)}
 			>
 				<HoverCardPrimitive.Arrow className="fill-current text-gray-200 dark:text-gray-800" />
 
-				<div className="flex h-full w-full space-x-4">
-					<div>
-						<h3 className="text-lg font-medium text-gray-900 dark:text-gray-100">{session.name}</h3>
+				<div className="h-full w-full">
+					<h3 className="text-lg font-medium text-gray-900 dark:text-gray-100">{session.name}</h3>
 
-						{session.venue && (
-							<div>
-								<Tooltip
-									side={'top'}
-									message={`This session is taking place at the ${session.venue.name} venue`}
-								>
-									<div className="inline-flex text-gray-800 flex-row items-center cursor-help">
-										<FontAwesomeIcon
-											fill="currentColor"
-											className="w-5 h-5 mr-1.5"
-											size="1x"
-											icon={faLocationDot}
+					{session.venue && (
+						<div>
+							<Tooltip
+								side={'top'}
+								message={`This session is taking place at the ${session.venue.name} venue`}
+							>
+								<div className="inline-flex text-gray-800 flex-row items-center cursor-help">
+									<FontAwesomeIcon
+										fill="currentColor"
+										className="w-5 h-5 mr-1.5"
+										size="1x"
+										icon={faLocationDot}
+									/>
+									{session.venue.name}
+								</div>
+							</Tooltip>
+						</div>
+					)}
+
+					{session.type && (
+						<div>
+							<Tooltip
+								side={'top'}
+								message={`This session is in the ${session.type.name} category`}
+							>
+								<div className="inline-flex text-gray-800 flex-row items-center cursor-help">
+									<div className="w-5 h-5 flex items-center justify-center mr-1.5">
+										<div
+											className="rounded-full w-3 h-3"
+											style={{
+												backgroundColor: session?.type?.color ?? '#888888'
+											}}
 										/>
-										{session.venue.name}
 									</div>
-								</Tooltip>
-							</div>
-						)}
+									{session.type.name}
+								</div>
+							</Tooltip>
+						</div>
+					)}
 
-						{session.type && (
-							<div>
-								<Tooltip
-									side={'top'}
-									message={`This session is in the ${session.type.name} category`}
-								>
-									<div className="inline-flex text-gray-800 flex-row items-center cursor-help">
-										<div className="w-5 h-5 flex items-center justify-center mr-1.5">
-											<div
-												className="rounded-full w-3 h-3"
-												style={{
-													backgroundColor: session?.type?.color ?? '#888888'
-												}}
-											/>
-										</div>
-										{session.type.name}
-									</div>
-								</Tooltip>
-							</div>
-						)}
+					{session.description && (
+						<p className="mt-1 text-sm font-normal text-gray-700 dark:text-gray-400">
+							{htmlToText(session.description)}
+						</p>
+					)}
 
-						{session.description && (
-							<p className="mt-1 text-sm font-normal text-gray-700 dark:text-gray-400">
-								{htmlToText(session.description)}
-							</p>
-						)}
-
+					<div className="flex flex-row justify-end w-full">
 						<Tooltip side={'top'} message={`Add the ${session.name} session to your schedule`}>
 							<Button
 								className="mt-2"
