87333a417e5ffc85577da4abc9571ea8e958bcf3
Jack Kelly <kcjackkelly@gmail.com>
2022-05-22 20:11:19 -0500

    impl pricing accordion

--- pages/pricing.tsx
+++ pages/pricing.tsx
@@ -9,6 +9,7 @@ import Link from 'next/link';
 import { LinkButton } from '../components/form/LinkButton';
 import { NextSeo } from 'next-seo';
 import { proAttendeePricing } from '../utils/const';
+import { PricingAccordion } from '../components/radix/components/PricingAccordion';
 
 const PricingPage: NextPage = () => {
 	const { control, watch } = useForm({ defaultValues: { attendees: 250 } });
@@ -40,7 +41,7 @@ const PricingPage: NextPage = () => {
 			<Column className="flex flex-col items-center">
 				<h1 className="text-2xl md:text-3xl font-bold">Single Event Plans & Pricing</h1>
 
-				<div className="flex flex-col items-center space-y-3 my-8">
+				<div className="flex flex-col items-center space-y-3 my-4">
 					<p className="text-lg">How many attendees do you expect?</p>
 					<Controller
 						control={control}
@@ -70,7 +71,7 @@ const PricingPage: NextPage = () => {
 						<strong>{attendees}</strong> Attendees
 					</p>
 				</div>
-				<div className="bg-white border-gray-300 border rounded shadow-sm p-5 flex flex-col justify-between items-center space-y-4 max-w-[450px] min-h-[350px]">
+				<div className="bg-white border-gray-300 border rounded shadow-sm p-5 flex flex-col justify-between items-center space-y-4 max-w-[450px] min-h-[350px] mt-3">
 					<div className="flex flex-row items-center">
 						<strong
 							className="text-2xl tracking-tight font-bold font-display mr-2"
@@ -101,19 +102,23 @@ const PricingPage: NextPage = () => {
 					</Link>
 				</div>
 			</Column>
-			<div className="bg-primary text-white my-10 md:my-0">
+
+			<div className="bg-primary text-white my-10 md:my-0 py-4">
 				<Column className="flex items-center justify-center">
-					<div className="flex flex-row flex-wrap items-center justify-center space-x-4 py-5">
-						<p className="text-sm md:text-lg text-center mb-3">
+					<div className="flex flex-row flex-wrap items-center justify-center space-x-4">
+						<p className="text-sm md:text-lg text-center my-1.5">
 							<span className="font-medium">Organizing multiple events?</span> Subscriptions are
 							available from $1000/year.
 						</p>
 						<Link href="/contact">
-							<LinkButton variant="inversePrimary">Contact Sales</LinkButton>
+							<LinkButton variant="inversePrimary" className="my-1.5">
+								Contact Sales
+							</LinkButton>
 						</Link>
 					</div>
 				</Column>
 			</div>
+
 			<Column>
 				<div className="flex flex-row items-center">
 					<strong
@@ -125,7 +130,7 @@ const PricingPage: NextPage = () => {
 					<span className="bg-primary text-white px-2 py-1 font-medium text-xs rounded">PRO</span>
 				</div>
 
-				<div className="mt-3 grid grid-cols-1 md:grid-cols-2 gap-6">
+				<div className="mt-3 grid grid-cols-1 lg:grid-cols-2 gap-6">
 					<div>
 						<h3 className="text-xl font-medium mb-1 border-b border-gray-300">Publish Features</h3>
 						<ul>
@@ -142,7 +147,7 @@ const PricingPage: NextPage = () => {
 						<ul>
 							<li>Use Evental with any webinar or video platform</li>
 							<li>Control access to webinar or video content</li>
-							<li>Allow attendees to update schedule to their time zone</li>
+							<li>Allow attendees to update Eventalule to their time zone</li>
 							<li>Provide post-event access to session recordings</li>
 						</ul>
 					</div>
@@ -157,6 +162,143 @@ const PricingPage: NextPage = () => {
 						</ul>
 					</div>
 				</div>
+			</Column>
+
+			<div className="bg-gray-100 my-10 md:my-0 py-4">
+				<Column className="grid grid-cols-4 md:gap-6">
+					<h3 className="text-2xl tracking-tight font-bold font-display mb-3 col-span-4 md:col-span-1">
+						Frequently Asked Questions
+					</h3>
+
+					<PricingAccordion
+						className="col-span-4 md:col-span-3"
+						items={[
+							{
+								header: 'Do you offer a nonprofit or education pricing?',
+								content: (
+									<p>
+										Yes, we offer nonprofit and educational pricing. You can view this here. Please
+										contact us if you would like to access this pricing.
+									</p>
+								)
+							},
+							{
+								header: 'Do you offer a 100% money-back guarantee?',
+								content: (
+									<p>
+										Yes, we offer a 60-day, 100% money-back guarantee if you and your event
+										administrators go through our support guides & video tutorials, view at least
+										one webinar recording, and set up your event.
+									</p>
+								)
+							},
+							{
+								header: 'What training, development or support do you offer?',
+								content: (
+									<p>
+										We offer a variety of training and development opportunities for you and your
+										team. This includes support guides, in-app. software tutorials, pre-recorded
+										training videos, live training/support sessions, and ongoing and on-demand
+										educational webinars.
+									</p>
+								)
+							},
+							{
+								header: 'What forms of payment do you accept?',
+								content: (
+									<p>
+										We accept credit cards, debit cards, ACH, wire transfers, purchase orders (POs),
+										and checks. POs/checks are only available for approved organizations. Payment
+										options are available after you sign up for a free trial.
+									</p>
+								)
+							},
+							{
+								header: 'Do you offer subscriptions?',
+								content: (
+									<p>
+										Yes, we offer subscriptions for organizations and individuals holding multiple
+										events over time. Please contact us for questions about subscriptions.
+									</p>
+								)
+							},
+							{
+								header: 'Do you offer payment plans?',
+								content: (
+									<p>
+										Yes, we offer payment plans for approved organizations and individuals. Payment
+										plans for approved organizations range from 30-90 days and typically require an
+										initial payment of at least 33% upon delivery of service.
+									</p>
+								)
+							},
+							{
+								header: 'Are you able to provide a quote?',
+								content: <p>Yes, we can provide a quote. Please Contact Us to arrange this.</p>
+							},
+							{
+								header: 'How does your pricing work?',
+								content: (
+									<p>
+										Pricing is based on 1) the plan you choose and 2) the number of attendees you
+										expect at your event. On each plan we have single event and annual subscription
+										options available. Contact us if you would like to discuss your options.
+									</p>
+								)
+							},
+							{
+								header: 'Are you GDPR and CCPA compliant?',
+								content: (
+									<p>
+										Yes, we are compliant with these and other privacy and protection practices.
+										Please contact us with any specific questions or concerns.
+									</p>
+								)
+							},
+							{
+								header: 'What is the maximum length of an event?',
+								content: (
+									<p>
+										The only limitation we have on length of your event is that, on a single event
+										plan, an event can be no longer than 1 month. This relates only to the actual
+										dates of the event itself (ie. from first to last session). You can have your
+										event live as long as you would like before and after for your attendees to
+										browse, sign up, access content etc. Contact Us for quote if you are looking at
+										hosting a longer event.
+									</p>
+								)
+							},
+							{
+								header: 'Do you offer setup and implementation assistance?',
+								content: (
+									<p>
+										Yes, we offer setup and implementation assistance. You can review our Support
+										Guides, contact our Support Team, or view our webinar recordings. If you wanted
+										additional and dedicated help, you can arrange this through Evental Event
+										Services.
+									</p>
+								)
+							},
+							{
+								header: 'Do you offer a free trial?',
+								content: (
+									<p>
+										Yes, we offer a 30-day free trial if you would like to test out Evental before
+										purchasing. You can start creating your event here. You can also contact us if
+										you require a longer free trial period. Note: You won’t have full functionality
+										and/or a live event whilst you’re in the free trial mode but if you did want to
+										test it in this capacity, contact us and we can set your event live for a few
+										days.
+									</p>
+								)
+							}
+						]}
+					/>
+				</Column>
+			</div>
+
+			<Column>
+				<h3 className="text-2xl font-bold">See why Evental is perfect for your event!</h3>
 			</Column>
 		</PageWrapper>
 	);
