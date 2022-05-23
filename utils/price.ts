17108ff3d5f2369541ff17750163556f93278a69
Jack Kelly <kcjackkelly@gmail.com>
2022-05-22 21:29:11 -0500

    lower base price

--- utils/price.ts
+++ utils/price.ts
@@ -1,13 +1,13 @@
 export const attendeesToPrice = (attendees: number): number => {
 	const step1 = attendees / 2;
 
-	const step2 = step1 + 300;
+	const step2 = step1 + 200;
 
 	return Math.round(step2 / 10) * 10;
 };
 
 export const priceToAttendees = (price: number): number => {
-	const step1 = price - 300;
+	const step1 = price - 200;
 
 	return step1 * 2;
 };
