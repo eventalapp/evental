2dff280b6fd96c097146e418f52a00ed0667d276
Jack Kelly <kcjackkelly@gmail.com>
2022-05-22 21:29:26 -0500

    column padding

--- components/layout/Column.tsx
+++ components/layout/Column.tsx
@@ -11,10 +11,10 @@ type LayoutProps = Props &
 	React.DetailedHTMLProps<React.HTMLAttributes<HTMLDivElement>, HTMLDivElement>;
 
 const variants = {
-	padding: 'w-11/12 lg:max-w-5xl md:w-4/6 sm:w-5/6 m-auto md:p-5 py-2 md:py-10',
+	padding: 'w-11/12 lg:max-w-5xl md:w-4/6 sm:w-5/6 m-auto md:p-5 py-7 md:py-12',
 	noPadding: 'w-11/12 lg:max-w-5xl md:w-4/6 sm:w-5/6 m-auto',
 	halfWidth:
-		'w-11/12 lg:max-w-4xl md:w-4/6 lg:w-1/2 2xl:w-4/12 sm:w-5/6 m-auto md:p-5 py-2 md:py-10'
+		'w-11/12 lg:max-w-4xl md:w-4/6 lg:w-1/2 2xl:w-4/12 sm:w-5/6 m-auto md:p-5 py-7 md:py-12'
 };
 
 const Column: React.FC<LayoutProps> = (props) => {
