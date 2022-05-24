3a11e39c78a88a9d34642e496d393afb370708fd
Jack Kelly <kcjackkelly@gmail.com>
2022-05-23 21:01:47 -0500

    not found page styling

--- pages/404.tsx
+++ pages/404.tsx
@@ -3,9 +3,11 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { NextPage } from 'next';
 import { NextSeo } from 'next-seo';
 import Link from 'next/link';
+import router from 'next/router';
 import React from 'react';
 
 import { Footer } from '../components/Footer';
+import { Button } from '../components/form/Button';
 import { LinkButton } from '../components/form/LinkButton';
 import Column from '../components/layout/Column';
 import PageWrapper from '../components/layout/PageWrapper';
@@ -22,14 +24,18 @@ const ErrorNotFoundPage: NextPage = () => {
 			<Navigation />
 
 			<Column>
-				<h1 className="text-2xl md:text-3xl font-bold">404 - Page not found</h1>
-
-				<Link href="/events" passHref>
-					<LinkButton className="mt-5" variant="primary">
-						Go home
-						<FontAwesomeIcon fill="currentColor" className="ml-2" size="1x" icon={faChevronRight} />
-					</LinkButton>
-				</Link>
+				<div className="flex items-center flex-col">
+					<h1 className="text-2xl md:text-3xl font-bold">Page not found</h1>
+					<p className="text-gray-600 my-5">We could not find the page you were looking for...</p>
+					<div className="flex flex-row space-x-4">
+						<Button type="button" variant="primary" onClick={router.back}>
+							Go Back
+						</Button>
+						<Link href="/" passHref>
+							<LinkButton variant="default">Return home</LinkButton>
+						</Link>
+					</div>
+				</div>
 			</Column>
 
 			<Footer />
