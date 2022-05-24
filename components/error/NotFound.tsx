f7f3e6f10e28c9cfa813f079d6a5444841a9b5c5
Jack Kelly <kcjackkelly@gmail.com>
2022-05-23 21:20:48 -0500

    notfound body text gray

--- components/error/NotFound.tsx
+++ components/error/NotFound.tsx
@@ -3,5 +3,5 @@ import React from 'react';
 export const NotFound: React.FC<{ message?: string }> = (props) => {
 	const { message = 'Not found.' } = props;
 
-	return <p className="my-3">{message}</p>;
+	return <p className="my-3 text-gray-600">{message}</p>;
 };
