6fa36d8ddb80ced1ed024d57ac6f66737e4f04da
Jack Kelly <kcjackkelly@gmail.com>
2022-05-25 21:58:23 -0500

    sendSupportTicketEmail.ts

---
+++ email/sendSupportTicketEmail.ts
@@ -0,0 +1,46 @@
+import { SESV2 } from 'aws-sdk';
+import { NextkitError } from 'nextkit';
+
+import { SubmitSupportTicketPayload } from '../utils/schemas';
+import { sendEmail } from '../utils/sendEmail';
+
+type SupportTicketEmailArgs = {
+	sendToAddress: string;
+	payload: SubmitSupportTicketPayload;
+};
+
+export const sendSupportTicketEmail = async (args: SupportTicketEmailArgs) => {
+	const body = `${Object.entries(args.payload)
+		.map(([key, value]) => (value ? `<p>${key}: ${value}</p>` : null))
+		.join('\n')}`;
+
+	const params: SESV2.SendEmailRequest = {
+		Content: {
+			Simple: {
+				Body: {
+					Html: {
+						Data: body,
+						Charset: 'UTF-8'
+					},
+					Text: {
+						Data: body,
+						Charset: 'UTF-8'
+					}
+				},
+				Subject: {
+					Data: `(${args.payload.name}) ${args.payload.attendanceType} ${args.payload.helpType} Support Ticket`,
+					Charset: 'UTF-8'
+				}
+			}
+		},
+		Destination: {
+			ToAddresses: [args.sendToAddress]
+		},
+		ReplyToAddresses: ['"Evental Support" <support@evental.app>'],
+		FromEmailAddress: '"Evental" <no-reply@evental.app>'
+	};
+
+	await sendEmail(params).catch((err) => {
+		throw new NextkitError(500, err);
+	});
+};
