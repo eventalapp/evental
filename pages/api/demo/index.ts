99ecb9c2a27ee66f2e211658601acd56c5a915cd
Jack Kelly <kcjackkelly@gmail.com>
2022-05-25 22:54:45 -0500

    sendDemoRequestEmail

---
+++ pages/api/demo/index.ts
@@ -0,0 +1,26 @@
+import { NextkitError } from 'nextkit';
+
+import { sendDemoRequestEmail } from '../../../email/sendDemoRequestEmail';
+import { api } from '../../../utils/api';
+import { SubmitDemoRequestSchema } from '../../../utils/schemas';
+
+export default api({
+	async POST({ ctx, req }) {
+		const user = await ctx.getUser();
+
+		if (!user?.id) {
+			throw new NextkitError(401, 'You must be logged in to do this.');
+		}
+
+		if (!user.emailVerified) {
+			throw new NextkitError(
+				401,
+				'You must verify your account to do this. Request a verification email in your user settings by clicking on your profile in the top right.'
+			);
+		}
+
+		const body = SubmitDemoRequestSchema.parse(req.body);
+
+		await sendDemoRequestEmail({ sendToAddress: 'support@evental.app', payload: body });
+	}
+});
