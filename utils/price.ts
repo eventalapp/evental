052fadc8cc9d9ca915eae0cc6ae4e6fc4d5f0a3f
Jack Kelly <kcjackkelly@gmail.com>
2022-05-28 12:22:24 -0500

    education attendee to price

--- utils/price.ts
+++ utils/price.ts
@@ -1,7 +1,15 @@
-export const attendeesToPrice = (attendees: number): number => {
+export const proAttendeesToPrice = (attendees: number): number => {
 	const step1 = attendees / 2;
 
-	const step2 = step1 + 200;
+	const step2 = step1 + 220;
+
+	return Math.round(step2 / 10) * 10;
+};
+
+export const eduAttendeesToPrice = (attendees: number): number => {
+	const step1 = attendees / 2.75;
+
+	const step2 = step1 + 110;
 
 	return Math.round(step2 / 10) * 10;
 };
