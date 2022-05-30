19415de2a105d4648bddfcc971b9f8823bc0f9ca
Jack Kelly <kcjackkelly@gmail.com>
2022-05-30 12:36:01 -0500

    unordered icon list

---
+++ components/guides/UnorderedIconList.tsx
@@ -0,0 +1,47 @@
+import { IconDefinition } from '@fortawesome/fontawesome-common-types';
+import { faFile } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import Link from 'next/link';
+import React from 'react';
+
+import { TableOfContentsItem } from './TableOfContents';
+
+export type UnorderedIconListItem = {
+	text: string;
+	relativeLink: string;
+	icon?: IconDefinition;
+};
+
+type Props = React.FC<
+	{ items: UnorderedIconListItem[] } & React.DetailedHTMLProps<
+		React.HTMLAttributes<HTMLUListElement>,
+		HTMLUListElement
+	>
+>;
+
+export const UnorderedIconList: Props = (props) => {
+	const { children, items, ...rest } = props;
+
+	return (
+		<ul className="pt-4 space-y-0.5" {...rest}>
+			{items.map((item, i) => {
+				const { text, relativeLink, icon = faFile } = item;
+
+				return (
+					<li key={`${text}-${i}`}>
+						<Link href={relativeLink}>
+							<a>
+								<FontAwesomeIcon
+									className="cursor-pointer pr-2 text-primary-400"
+									size="1x"
+									icon={icon}
+								/>
+								{text}
+							</a>
+						</Link>
+					</li>
+				);
+			})}
+		</ul>
+	);
+};
