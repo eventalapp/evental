b82286438a33a0bd656ed623b218e77a8f8ba355
Jack Kelly <kcjackkelly@gmail.com>
2022-05-30 12:18:46 -0500

    table of contents

---
+++ components/guides/TableOfContents.tsx
@@ -0,0 +1,33 @@
+import Link from 'next/link';
+import React from 'react';
+
+export type TableOfContentsItem = {
+	text: string;
+	relativeLink: string;
+};
+
+type Props = React.FC<
+	{ items: TableOfContentsItem[] } & React.DetailedHTMLProps<
+		React.HTMLAttributes<HTMLUListElement>,
+		HTMLUListElement
+	>
+>;
+
+export const TableOfContents: Props = (props) => {
+	const { children, items, ...rest } = props;
+
+	return (
+		<>
+			<h3 className="font-bold text-xl mb-2">Table of contents</h3>
+			<ul className="list-disc pl-5 space-y-0.5 text-gray-700" {...rest}>
+				{items.map((item, i) => (
+					<li key={`${item.text}-${i}`}>
+						<Link href={item.relativeLink}>
+							<a>{item.text}</a>
+						</Link>
+					</li>
+				))}
+			</ul>
+		</>
+	);
+};
