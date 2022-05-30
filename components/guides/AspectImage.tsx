b2df166ba5693b80680dca94750512fa08005e0c
Jack Kelly <kcjackkelly@gmail.com>
2022-05-30 13:44:03 -0500

    aspect image

--- components/guides/AspectImage.tsx
+++ components/guides/AspectImage.tsx
@@ -1,19 +1,26 @@
 import * as AspectRatio from '@radix-ui/react-aspect-ratio';
+import classNames from 'classnames';
 import Image from 'next/image';
 import React from 'react';
 
 type Props = React.FC<
-	{ imageUrl: string; alt: string; ratio: number } & React.DetailedHTMLProps<
+	{ className?: string; imageUrl: string; alt: string; ratio: number } & React.DetailedHTMLProps<
 		React.HTMLAttributes<HTMLDivElement>,
 		HTMLDivElement
 	>
 >;
 
 export const AspectImage: Props = (props) => {
-	const { children, imageUrl, alt, ratio, ...rest } = props;
+	const { className, children, imageUrl, alt, ratio, ...rest } = props;
 
 	return (
-		<div className="w-full relative border border-gray-200 shadow-sm rounded-md" {...rest}>
+		<div
+			className={classNames(
+				className,
+				'w-full relative border border-gray-200 shadow-sm rounded-md'
+			)}
+			{...rest}
+		>
 			<AspectRatio.Root ratio={ratio}>
 				<Image alt={alt} src={imageUrl} className="rounded-md" layout="fill" />
 			</AspectRatio.Root>
