26c060b1d467a05877433889f24ab3fb8036da85
Jack Kelly <kcjackkelly@gmail.com>
2022-06-05 11:45:19 -0700

    send event message route

---
+++ pages/api/events/[eid]/admin/messages/index.ts
@@ -0,0 +1,35 @@
+import { NextkitError } from 'nextkit';
+import { getEvent } from '../..';
+import { sendEventMessageEmail } from '../../../../../../email/sendEventMessageEmail';
+import { prisma } from '../../../../../../prisma/client';
+import { api } from '../../../../../../utils/api';
+import { SendEventMessageSchema } from '../../../../../../utils/schemas';
+
+export default api({
+	async POST({ ctx, req }) {
+		const body = SendEventMessageSchema.parse(req.body);
+
+		const event = await getEvent(String(body.eventId));
+
+		if (!event) {
+			throw new NextkitError(404, 'Event not found');
+		}
+
+		await prisma.eventMessage.create({
+			data: {
+				body: body.body,
+				title: body.title,
+				eventId: body.eventId,
+				sentBy: body.sentBy
+			}
+		});
+
+		await sendEventMessageEmail({
+			event,
+			sendToAddresses: ['f'],
+			body: body.body,
+			title: body.title,
+			sentBy: body.sentBy
+		});
+	}
+});
