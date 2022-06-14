bc0195c874d8fe52cba7ef49946fa49e3637ab23
Jack Kelly <kcjackkelly@gmail.com>
2022-06-14 13:22:04 -0700

    color links

--- components/navigation/LinkItem.tsx
+++ components/navigation/LinkItem.tsx
@@ -3,25 +3,30 @@ import classNames from 'classnames';
 import Link from 'next/link';
 import { useRouter } from 'next/router';
 import React from 'react';
+import { theme } from '../../tailwind.config';
 
-export const LinkItem: React.FC<{ link: string; label: string }> = (props) => {
-	const { link, label } = props;
+export const LinkItem: React.FC<{ link: string; label: string; color?: string }> = (props) => {
+	const { link, label, color } = props;
 	const router = useRouter();
 
 	return (
 		<Link href={link} passHref>
 			<NavigationMenuPrimitive.Link
-				className={classNames(
-					'cursor-pointer pb-1.5 border-b-2',
-					router.asPath == link ? 'border-primary' : 'border-transparent'
-				)}
+				className={classNames('cursor-pointer pb-1.5 border-b-2')}
+				style={{
+					borderColor:
+						router.asPath == link ? color ?? theme.extend.colors.primary.DEFAULT : 'transparent'
+				}}
 			>
 				<NavigationMenuPrimitive.Item
 					className={classNames(
 						'px-3 py-1.5 text-sm rounded-md hover:bg-gray-75 dark:hover:bg-gray-900',
-						'text-sm font-medium dark:text-gray-75',
-						router.asPath == link ? 'text-primary' : 'text-gray-900'
+						'text-sm font-medium dark:text-gray-75'
 					)}
+					style={{
+						color:
+							router.asPath == link ? color ?? theme.extend.colors.primary.DEFAULT : 'text-gray-900'
+					}}
 				>
 					{label}
 				</NavigationMenuPrimitive.Item>
