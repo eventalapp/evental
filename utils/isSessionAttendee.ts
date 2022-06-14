4941849719953bb5f3ba20448bf2b51074d1355b
Jack Kelly <kcjackkelly@gmail.com>
2022-06-14 14:21:21 -0700

    isSessionAttendee

--- utils/isSessionAttendee.ts
+++ utils/isSessionAttendee.ts
@@ -1,9 +1,18 @@
 import { getEvent } from '../pages/api/events/[eid]';
 import { getAttendee } from '../pages/api/events/[eid]/attendees/[uid]';
+import { getSession } from '../pages/api/events/[eid]/sessions/[sid]';
 import { prisma } from '../prisma/client';
 
-export const isSessionAttendee = async (userId: string, eventId: string, sessionId: string) => {
-	const event = await getEvent(String(eventId));
+export type IsSessionAttendeeArguments = {
+	eid: string;
+	sid: string;
+	userId: string;
+};
+
+export const isSessionAttendee = async (args: IsSessionAttendeeArguments) => {
+	const { eid, sid, userId } = args;
+
+	const event = await getEvent(String(eid));
 
 	if (!event) {
 		return false;
@@ -15,19 +24,21 @@ export const isSessionAttendee = async (userId: string, eventId: string, session
 		return false;
 	}
 
+	const session = await getSession(event.id, sid);
+
+	if (!session) {
+		return false;
+	}
+
 	const sessionAttendee = await prisma.eventSessionAttendee.findFirst({
 		where: {
-			sessionId: sessionId,
+			sessionId: session.id,
 			attendeeId: attendee.id,
 			event: {
 				id: event.id
 			}
 		}
 	});
-
-	if (!sessionAttendee) {
-		return false;
-	}
 
 	return Boolean(sessionAttendee);
 };
