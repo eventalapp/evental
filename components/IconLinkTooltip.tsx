11af4dd15bb2ad8cf5043971d9d35666cb00acc7
Jack Kelly <kcjackkelly@gmail.com>
2022-06-14 21:31:01 -0700

    Icon Link tooltip

---
+++ components/IconLinkTooltip.tsx
@@ -0,0 +1,30 @@
+import { IconDefinition } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import Link from 'next/link';
+import React from 'react';
+import Tooltip from './radix/components/Tooltip';
+
+type Props = React.FC<
+	{
+		href: string;
+		icon: IconDefinition;
+		message: string;
+		side?: 'bottom' | 'top' | 'right' | 'left' | undefined;
+	} & React.DetailedHTMLProps<React.AnchorHTMLAttributes<HTMLAnchorElement>, HTMLAnchorElement>
+>;
+
+export const IconLinkTooltip: Props = (props) => {
+	const { children, href, icon, message, side, ...rest } = props;
+
+	return (
+		<Tooltip message={message} side={side}>
+			<div className="p-1 -mr-1 inline-block">
+				<Link href={href} passHref>
+					<a {...rest}>
+						<FontAwesomeIcon fill="currentColor" className="h-6 w-6" size="1x" icon={icon} />
+					</a>
+				</Link>
+			</div>
+		</Tooltip>
+	);
+};
