e496770d7c1247bd8a0fd681082aa90ad8708e24
Jack Kelly <kcjackkelly@gmail.com>
2022-06-19 14:49:58 -0500

    switch border size

--- components/radix/components/Switch.tsx
+++ components/radix/components/Switch.tsx
@@ -13,7 +13,7 @@ const Switch = (props: Props) => {
 				'group',
 				'radix-state-checked:bg-primary',
 				'radix-state-unchecked:bg-gray-200 dark:radix-state-unchecked:bg-gray-800',
-				'relative inline-flex h-[24px] w-[44px] flex-shrink-0 cursor-pointer rounded-full border border-transparent transition-colors duration-200 ease-in-out',
+				'relative inline-flex h-[24px] w-[44px] flex-shrink-0 cursor-pointer rounded-full border-2 border-transparent transition-colors duration-200 ease-in-out',
 				'focus:outline-none focus-visible:ring focus-visible:ring-primary-500 focus-visible:ring-opacity-75'
 			)}
 		>
