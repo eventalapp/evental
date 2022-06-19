258694d33d3318bc3d7fa402f66b9c174dca1192
Jack Kelly <kcjackkelly@gmail.com>
2022-06-19 14:46:08 -0500

    custom ring color input

--- components/form/Input.tsx
+++ components/form/Input.tsx
@@ -1,22 +1,32 @@
 import classNames from 'classnames';
 import React from 'react';
+import { theme } from '../../tailwind.config';
 
 const variants = {
 	secondary:
-		'w-full px-3 py-2 text-sm font-medium text-gray-700 bg-white border border-gray-300 rounded shadow-sm hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-0 focus:ring-primary'
+		'w-full px-3 py-2 text-sm font-medium text-gray-700 bg-white border border-gray-300 rounded shadow-sm hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-0 focus:ring-primary focus:border-gray-300'
 };
 
 type Props = {
 	className?: string;
 	variant?: keyof typeof variants;
+	color?: string;
 	[x: string]: unknown;
 } & React.DetailedHTMLProps<React.InputHTMLAttributes<HTMLInputElement>, HTMLInputElement>;
 
 export const Input = React.forwardRef<HTMLInputElement, Props>((props, ref) => {
-	const { className, children, variant = 'secondary', ...rest } = props;
+	const { className, children, variant = 'secondary', color, ...rest } = props;
 
 	return (
-		<input className={classNames(variants[variant], className)} ref={ref} {...rest}>
+		<input
+			className={classNames(variants[variant], className)}
+			ref={ref}
+			style={{
+				// @ts-ignore
+				'--tw-ring-color': color ?? theme.extend.colors.primary.DEFAULT
+			}}
+			{...rest}
+		>
 			{children}
 		</input>
 	);
