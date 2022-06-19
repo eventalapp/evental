9d740f2930967ae1dbb4974f6471070cedd4cff0
Jack Kelly <kcjackkelly@gmail.com>
2022-06-19 14:45:35 -0500

    text area custom color ring

--- components/form/Textarea.tsx
+++ components/form/Textarea.tsx
@@ -1,22 +1,32 @@
 import classNames from 'classnames';
 import React from 'react';
+import { theme } from '../../tailwind.config';
 
 const variants = {
 	secondary:
-		'w-full px-3 py-2 text-sm font-medium text-gray-700 bg-white border border-gray-300 rounded shadow-sm hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-0 focus:ring-primary'
+		'w-full px-3 py-2 text-sm font-medium text-gray-700 bg-white border border-gray-300 rounded shadow-sm hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-0 focus:ring-primary focus:border-gray-300'
 };
 
 type Props = {
 	className?: string;
 	variant?: keyof typeof variants;
+	color?: string;
 	[x: string]: unknown;
 } & React.DetailedHTMLProps<React.TextareaHTMLAttributes<HTMLTextAreaElement>, HTMLTextAreaElement>;
 
 export const Textarea = React.forwardRef<HTMLTextAreaElement, Props>((props, ref) => {
-	const { className, children, variant = 'secondary', ...rest } = props;
+	const { className, children, variant = 'secondary', color, ...rest } = props;
 
 	return (
-		<textarea className={classNames(variants[variant], className)} ref={ref} {...rest}>
+		<textarea
+			className={classNames(variants[variant], className)}
+			style={{
+				// @ts-ignore
+				'--tw-ring-color': color ?? theme.extend.colors.primary.DEFAULT
+			}}
+			ref={ref}
+			{...rest}
+		>
 			{children}
 		</textarea>
 	);
