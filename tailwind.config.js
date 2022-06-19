7ec95bb045523300ddb379c5866d6263cfde396c
Jack Kelly <kcjackkelly@gmail.com>
2022-06-19 14:45:20 -0500

    add red colors to tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -152,6 +152,19 @@ module.exports = {
 				eventDesktopList: '80px 80px 1fr'
 			},
 			colors: {
+				red: {
+					DEFAULT: '#EF4444',
+					50: '#FEF2F2',
+					100: '#FEE2E2',
+					200: '#FECACA',
+					300: '#FCA5A5',
+					400: '#F87171',
+					500: '#EF4444',
+					600: '#DC2626',
+					700: '#B91C1C',
+					800: '#991B1B',
+					900: '#7F1D1D'
+				},
 				gray: {
 					DEFAULT: '#475569',
 					50: '#f8fafc',
