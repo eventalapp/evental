72b77e896dda2c614ae08750fc5f14c128f31c0a
Jack Kelly <kcjackkelly@gmail.com>
2022-06-19 19:33:35 -0500

    remove shadow sm off of event logo

--- components/form/DatePickerButton.tsx
+++ components/form/DatePickerButton.tsx
@@ -11,7 +11,7 @@ export const DatePickerButton = forwardRef<
 			onClick={onClick}
 			ref={ref}
 			type="button"
-			className="inline-flex w-full justify-start rounded border border-gray-300 bg-white py-2 px-3 text-base font-medium text-gray-700 shadow-sm hover:bg-gray-50 focus:border-gray-300 focus:outline-none focus:ring-2 focus:ring-primary focus:ring-offset-0"
+			className="duration-50 inline-flex w-full justify-start rounded border border-gray-300 bg-white py-2 px-3 text-base font-medium text-gray-700 shadow-sm transition hover:bg-gray-50 focus:border-gray-300 focus:outline-none focus:ring-2 focus:ring-primary focus:ring-offset-0"
 			style={{
 				// @ts-ignore
 				'--tw-ring-color': color ?? theme.extend.colors.primary.DEFAULT
