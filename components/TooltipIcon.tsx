5d4e67915ed10bff0a842d62bd689187ab691772
Jack Kelly <kcjackkelly@gmail.com>
2022-06-21 19:30:42 -0500

    Tooltip icon component

---
+++ components/TooltipIcon.tsx
@@ -0,0 +1,73 @@
+import { IconDefinition } from '@fortawesome/fontawesome-common-types';
+import { faQuestion } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import classNames from 'classnames';
+import React from 'react';
+
+import Tooltip from './radix/components/Tooltip';
+
+export const TooltipIcon: React.FC<{
+	icon?: IconDefinition;
+	tooltipMessage: string;
+	label: string;
+	iconClassName?: string;
+	wrapperClassName?: string;
+	externalLink?: string;
+	customIcon?: React.ReactNode;
+}> = (props) => {
+	const {
+		icon = faQuestion,
+		tooltipMessage,
+		label,
+		iconClassName,
+		wrapperClassName,
+		externalLink,
+		customIcon
+	} = props;
+
+	const finalIcon = customIcon ? (
+		customIcon
+	) : (
+		<FontAwesomeIcon
+			fill="currentColor"
+			className={classNames('mr-1.5 h-4 w-4 text-gray-500', iconClassName)}
+			size="1x"
+			icon={icon}
+		/>
+	);
+
+	if (externalLink) {
+		return (
+			<Tooltip message={tooltipMessage}>
+				<a href={externalLink} target="_blank" rel="noopener noreferrer">
+					<div
+						className={classNames(
+							externalLink ? 'cursor-pointer' : 'cursor-help',
+							'mr-3 mb-1 flex flex-row items-center text-sm md:text-base',
+							wrapperClassName
+						)}
+					>
+						{finalIcon}
+
+						<p>{label}</p>
+					</div>
+				</a>
+			</Tooltip>
+		);
+	}
+
+	return (
+		<Tooltip message={tooltipMessage}>
+			<div
+				className={classNames(
+					'mr-3 mb-1 flex cursor-help flex-row items-center text-sm md:text-base',
+					wrapperClassName
+				)}
+			>
+				{finalIcon}
+
+				<p>{label}</p>
+			</div>
+		</Tooltip>
+	);
+};
