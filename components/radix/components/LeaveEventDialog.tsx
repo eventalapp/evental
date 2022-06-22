1f8063cd010ae7f729a643e72fd0ec07bfdf01db
Jack Kelly <kcjackkelly@gmail.com>
2022-06-22 11:42:29 -0500

    use Button components in  leave event dialog

--- components/radix/components/LeaveEventDialog.tsx
+++ components/radix/components/LeaveEventDialog.tsx
@@ -4,6 +4,7 @@ import cx from 'classnames';
 import React, { Fragment, useState } from 'react';
 
 import { useLeaveEvent } from '../../../hooks/mutations/useLeaveEvent';
+import { Button } from '../../form/Button';
 
 interface Props {
 	eventSlug: string;
@@ -60,28 +61,18 @@ const LeaveEventDialog: React.FC<Props> = (props) => {
 							removed.
 						</AlertDialogPrimitive.Description>
 						<div className="mt-4 flex justify-end space-x-2">
-							<AlertDialogPrimitive.Cancel
-								className={cx(
-									'inline-flex select-none justify-center rounded-md px-4 py-2 text-sm font-medium',
-									'bg-white text-gray-900 hover:bg-gray-50 dark:bg-gray-700 dark:text-gray-100 hover:dark:bg-gray-600',
-									'border border-gray-300 dark:border-transparent',
-									'focus:outline-none focus-visible:ring focus-visible:ring-red-500 focus-visible:ring-opacity-75'
-								)}
-							>
-								Cancel
+							<AlertDialogPrimitive.Cancel>
+								<Button variant="no-bg">Cancel</Button>
 							</AlertDialogPrimitive.Cancel>
-							<AlertDialogPrimitive.Action
-								className={cx(
-									'inline-flex select-none justify-center rounded-md px-4 py-2 text-sm font-medium',
-									'bg-red-600 text-white hover:bg-red-700 dark:bg-red-700 dark:text-gray-100 dark:hover:bg-red-600',
-									'border border-transparent',
-									'focus:outline-none focus-visible:ring focus-visible:ring-red-500 focus-visible:ring-opacity-75'
-								)}
-								onClick={() => {
-									leaveEventMutation.mutate();
-								}}
-							>
-								Confirm
+							<AlertDialogPrimitive.Action>
+								<Button
+									variant="danger"
+									onClick={() => {
+										leaveEventMutation.mutate();
+									}}
+								>
+									Confirm
+								</Button>
 							</AlertDialogPrimitive.Action>
 						</div>
 					</AlertDialogPrimitive.Content>
