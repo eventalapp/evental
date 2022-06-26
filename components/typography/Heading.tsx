5b2b76e2b21d29bbcd4a93995737cea1d7b81532
Jack Kelly <kcjackkelly@gmail.com>
2022-06-25 20:49:02 -0500

    impl heading variants

--- components/typography/Heading.tsx
+++ components/typography/Heading.tsx
@@ -1,23 +1,18 @@
 import classNames from 'classnames';
 import React from 'react';
 
-type Props = { className?: string; level?: 1 | 2 | 3 | 5 | 6 };
+const variants = {
+	default: 'text-2xl font-bold leading-[1.2] tracking-tight md:text-3xl'
+};
+
+type Props = { className?: string; level?: 1 | 2 | 3 | 5 | 6; variant?: keyof typeof variants };
 
 type HeadingTag = 'h1' | 'h2' | 'h3' | 'h4' | 'h5' | 'h6';
 
 export const Heading: React.FC<Props> = (props) => {
-	const { children, className, level = 1 } = props;
+	const { children, className, level = 1, variant = 'default' } = props;
 
 	const Tag = `h${level}` as HeadingTag;
 
-	return (
-		<Tag
-			className={classNames(
-				'text-2xl font-bold leading-[1.2] tracking-tight md:text-3xl',
-				className
-			)}
-		>
-			{children}
-		</Tag>
-	);
+	return <Tag className={classNames(variants[variant], className)}>{children}</Tag>;
 };
