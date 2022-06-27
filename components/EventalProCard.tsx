8af220d901e73867bc3bda93e7e1e54a8ab6bba8
Jack Kelly <kcjackkelly@gmail.com>
2022-06-27 18:01:19 -0500

    evental pro card componentization

--- components/EventalProCard.tsx
+++ components/EventalProCard.tsx
@@ -11,6 +11,82 @@ type EventalProCardProps = {
 	isEducation?: boolean;
 };
 
+const EventalProCardPromotion: React.FC = () => {
+	if (sale.percentage > 0 || sale.flatAmount > 0) {
+		return (
+			<div className="flex flex-row items-center my-3 justify-center">
+				<div className="flex items-center justify-center w-10 h-10 relative mr-1">
+					<FontAwesomeIcon
+						fill="currentColor"
+						className="h-3.5 w-3.5 text-white absolute z-10"
+						size="1x"
+						icon={faPercent}
+					/>
+					<FontAwesomeIcon
+						fill="currentColor"
+						className="h-8 w-8 text-green-500 absolute animate-spin-slow"
+						size="1x"
+						icon={faBadge}
+					/>
+				</div>
+				<div>
+					<p className="text-green-500 font-bold leading-[1.2] tracking-tight">
+						Promotional Offer!
+					</p>
+					{sale.percentage > 0 && (
+						<p className="text-gray-700 text-sm">{sale.percentage}% off until August 31st, 2022.</p>
+					)}
+					{sale.flatAmount > 0 && (
+						<span className="text-gray-700 text-sm">
+							${sale.flatAmount} off until August 31st, 2022.
+						</span>
+					)}
+				</div>
+			</div>
+		);
+	}
+
+	return null;
+};
+
+const EventalProCardPrice: React.FC<EventalProCardProps> = (props) => {
+	const { attendees, isEducation = false } = props;
+
+	if (sale.percentage > 0 || sale.flatAmount > 0) {
+		return (
+			<div className="flex flex-row justify-center items-end -ml-4">
+				<p className="inline-block">
+					<span className="mr-2 line-through text-lg md:text-2xl text-gray-500 font-medium leading-[1.2] tracking-tight">
+						<span className="text-sm md:text-lg align-top">$</span>
+						{isEducation
+							? eduAttendeePricing[attendees].price
+							: proAttendeePricing[attendees].price}
+					</span>
+				</p>
+				<p className="inline-block">
+					<span className="text-green-500 text-3xl font-bold md:text-5xl leading-[1]">
+						<span className="text-xl md:text-2xl align-top">$</span>
+						{isEducation
+							? priceAfterSale(eduAttendeePricing[attendees].price)
+							: priceAfterSale(proAttendeePricing[attendees].price)}
+					</span>
+				</p>
+			</div>
+		);
+	}
+
+	return (
+		<div className="flex flex-row justify-center items-end -ml-4 mb-3">
+			<p className="inline-block">
+				<span className="text-gray-700 text-3xl font-bold md:text-5xl leading-[1]">
+					<span className="text-xl md:text-2xl align-top">$</span>
+					{isEducation ? eduAttendeePricing[attendees].price : proAttendeePricing[attendees].price}
+				</span>
+			</p>
+		</div>
+	);
+};
+
 export const EventalProCard: React.FC<EventalProCardProps> = (props) => {
 	const { attendees, isEducation = false, children } = props;
 
@@ -35,52 +111,9 @@ export const EventalProCard: React.FC<EventalProCardProps> = (props) => {
 			</p>
 
 			<div>
-				<p className="text-center">
-					<span className="mr-2 line-through text-2xl text-gray-500 font-medium leading-[1.2] tracking-tight">
-						<span className="text-lg align-text-top">$</span>
-						{isEducation
-							? eduAttendeePricing[attendees].price
-							: proAttendeePricing[attendees].price}
-					</span>
-					<span className="text-green-500 text-3xl font-bold md:text-5xl leading-[1]">
-						<span className="text-2xl leading-[1.7] align-text-top">$</span>
-						{isEducation
-							? priceAfterSale(eduAttendeePricing[attendees].price)
-							: priceAfterSale(proAttendeePricing[attendees].price)}
-					</span>
-				</p>
+				<EventalProCardPrice attendees={attendees} isEducation={isEducation} />
 
-				<div className="flex flex-row items-center my-3 justify-center">
-					<div className="flex items-center justify-center w-10 h-10 relative mr-1">
-						<FontAwesomeIcon
-							fill="currentColor"
-							className="h-3.5 w-3.5 text-white absolute z-10"
-							size="1x"
-							icon={faPercent}
-						/>
-						<FontAwesomeIcon
-							fill="currentColor"
-							className="h-8 w-8 text-green-500 absolute animate-spin-slow"
-							size="1x"
-							icon={faBadge}
-						/>
-					</div>
-					<div>
-						<p className="text-green-500 font-bold leading-[1.2] tracking-tight">
-							Promotional Offer!
-						</p>
-						{sale.percentage > 0 && (
-							<p className="text-gray-700 text-sm">
-								{sale.percentage}% off until August 31st, 2022.
-							</p>
-						)}
-						{sale.flatAmount > 0 && (
-							<span className="text-gray-700 text-sm">
-								${sale.flatAmount} off until August 31st, 2022.
-							</span>
-						)}
-					</div>
-				</div>
+				<EventalProCardPromotion />
 
 				<p className="text-center text-sm text-gray-600">
 					Includes {attendees} attendees (
