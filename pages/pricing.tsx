0e566361cb6dffd1df24492d108d99dd12cbb60e
Jack Kelly <kcjackkelly@gmail.com>
2022-06-27 11:45:30 -0500

    add sale on pricing page

--- pages/pricing.tsx
+++ pages/pricing.tsx
@@ -13,7 +13,7 @@ import { PricingAccordion } from '../components/radix/components/PricingAccordio
 import Slider from '../components/radix/components/Slider';
 import Button from '../components/radix/components/shared/Button';
 import { Heading } from '../components/typography/Heading';
-import { eduAttendeePricing, proAttendeePricing } from '../utils/const';
+import { eduAttendeePricing, priceAfterSale, proAttendeePricing, sale } from '../utils/const';
 
 const PricingPage: NextPage = () => {
 	const { control, watch } = useForm({ defaultValues: { attendees: 250 } });
@@ -92,21 +92,42 @@ const PricingPage: NextPage = () => {
 					</p>
 
 					<div>
-						<p className="text-center text-2xl font-bold md:text-3xl">
+						<p className="text-center text-2xl font-bold md:text-3xl flex flex-row items-start justify-center">
 							<span className="mr-0.5 align-text-top text-lg text-gray-700">$</span>
-							{isEducation
-								? eduAttendeePricing[attendees].price
-								: proAttendeePricing[attendees].price}
+							<span className="mr-2">
+								{isEducation
+									? priceAfterSale(eduAttendeePricing[attendees].price)
+									: priceAfterSale(proAttendeePricing[attendees].price)}
+							</span>
+
+							{sale.percentage > 0 && (
+								<span className="text-tiny bg-green-400 text-white p-1 rounded-md">
+									{sale.percentage}% OFF
+								</span>
+							)}
+							{sale.flatAmount > 0 && (
+								<span className="text-tiny bg-green-400 text-white p-1 rounded-md">
+									${sale.flatAmount} OFF
+								</span>
+							)}
 						</p>
 						<p className="text-center text-sm text-gray-600">
 							Includes {attendees} attendees (
 							{isEducation
-								? eduAttendeePricing[attendees].perAttendeePrice < 1
-									? `${Math.ceil(eduAttendeePricing[attendees].perAttendeePrice * 100)}¢`
-									: `$${eduAttendeePricing[attendees].perAttendeePrice.toFixed(2)}`
-								: proAttendeePricing[attendees].perAttendeePrice < 1
-								? `${Math.ceil(proAttendeePricing[attendees].perAttendeePrice * 100)}¢`
-								: `$${proAttendeePricing[attendees].perAttendeePrice.toFixed(2)}`}
+								? priceAfterSale(eduAttendeePricing[attendees].price) / attendees < 1
+									? `${Math.ceil(
+											(priceAfterSale(eduAttendeePricing[attendees].price) / attendees) * 100
+									  )}¢`
+									: `$${(priceAfterSale(eduAttendeePricing[attendees].price) / attendees).toFixed(
+											2
+									  )}`
+								: priceAfterSale(proAttendeePricing[attendees].price) / attendees < 1
+								? `${Math.ceil(
+										(priceAfterSale(proAttendeePricing[attendees].price) / attendees) * 100
+								  )}¢`
+								: `$${(priceAfterSale(proAttendeePricing[attendees].price) / attendees).toFixed(
+										2
+								  )}`}
 							/per)
 						</p>
 					</div>
@@ -429,3 +450,4 @@ const PricingPage: NextPage = () => {
 };
 
 export default PricingPage;
+
