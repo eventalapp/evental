cdd1832bae68b4d6b0db391ea9233eb2783afcd1
Jack Kelly <kcjackkelly@gmail.com>
2022-06-27 20:21:12 -0500

    remove unused products api route

--- pages/api/products/index.ts
+++
@@ -1,9 +0,0 @@
-import { api } from '../../../utils/api';
-
-export default api({
-	async GET() {
-		const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY!);
-
-		return await stripe.products.list();
-	}
-});
