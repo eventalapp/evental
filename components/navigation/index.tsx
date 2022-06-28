8854171b3c0556fb286d8004bf0ee7d75a44ca38
Jack Kelly <kcjackkelly@gmail.com>
2022-06-28 16:29:29 -0500

    more navigation consolidation

--- components/navigation/index.tsx
+++ components/navigation/index.tsx
@@ -1,10 +1,20 @@
+import { faXmark } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import * as NavigationMenuPrimitive from '@radix-ui/react-navigation-menu';
+import classNames from 'classnames';
 import Link from 'next/link';
 import React, { useState } from 'react';
+import Skeleton from 'react-loading-skeleton';
 
 import { useSignOutMutation } from '../../hooks/mutations/useSignOutMutation';
 import { useUser } from '../../hooks/queries/useUser';
-import { Authenticated } from './Authenticated';
-import { Unauthenticated } from './Unauthenticated';
+import { faBarsSquare } from '../../icons';
+import { LinkButton } from '../form/LinkButton';
+import { ProfileDropdown } from '../radix/components/ProfileDropdown';
+import { FullscreenLinkItem } from './FullscreenLinkItem';
+import { LinkItem } from './LinkItem';
+import { LogoLinkItem } from './LogoLinkItem';
+import { NavigationWrapper } from './NavigationWrapper';
 
 type NavigationLink = {
 	link: string;
@@ -28,30 +38,170 @@ const FreeEventalPro = (
 );
 
 export const Navigation: React.FC<Props> = (props) => {
-	const { className, ...restProps } = props;
+	const { className } = props;
 	const [isOpen, setIsOpen] = useState(false);
 	const { signOutMutation } = useSignOutMutation();
-	const { user } = useUser();
-
-	if (user) {
-		return (
-			<>
-				{FreeEventalPro}
-				<Authenticated
-					signOutMutation={signOutMutation}
-					user={user}
-					isOpen={isOpen}
-					setIsOpen={setIsOpen}
-					{...restProps}
-				/>
-			</>
-		);
-	}
+	const { user, isUserLoading } = useUser();
 
 	return (
 		<>
 			{FreeEventalPro}
-			<Unauthenticated isOpen={isOpen} setIsOpen={setIsOpen} {...restProps} />
+			<NavigationWrapper>
+				<NavigationMenuPrimitive.Root className="relative w-full">
+					<NavigationMenuPrimitive.List className="m-auto flex h-14 w-full max-w-7xl items-center justify-between px-3">
+						<div>
+							<LogoLinkItem />
+						</div>
+
+						<div className="hidden h-full flex-row justify-end lg:flex">
+							<div className="flex flex-row items-end">
+								<LinkItem link={`/pricing`} label={'Pricing'} />
+								<LinkItem link={`/contact`} label={'Contact'} />
+								<LinkItem link={`/guides`} label={'Guides'} />
+								<LinkItem link={`/events`} label={'Events'} />
+							</div>
+						</div>
+
+						<div className="flex flex-row space-x-8 font-medium lg:hidden">
+							<FontAwesomeIcon
+								className="cursor-pointer text-gray-900"
+								size="2x"
+								fill="currentColor"
+								icon={faBarsSquare}
+								onClick={() => {
+									setIsOpen(true);
+								}}
+							/>
+						</div>
+
+						<div className="hidden h-full flex-row justify-end lg:flex">
+							<div className="flex flex-row items-end">
+								{isUserLoading
+									? // <Skeleton className="w-20 h-7 mb-2 mx-2" />
+									  null
+									: user === undefined && <LinkItem link={`/auth/signin`} label={'Sign in'} />}
+							</div>
+							<div className="flex flex-row items-center">
+								{user ? (
+									<>
+										<NavigationMenuPrimitive.Item className="mr-6">
+											<Link href="/events/create" passHref>
+												<LinkButton>Create Event</LinkButton>
+											</Link>
+										</NavigationMenuPrimitive.Item>
+										<NavigationMenuPrimitive.Item className="flex">
+											<ProfileDropdown user={user} signOutMutation={signOutMutation} />
+										</NavigationMenuPrimitive.Item>
+									</>
+								) : (
+									isUserLoading && (
+										<>
+											{/*<Skeleton className="w-32 h-9 mb-2 mr-6" />*/}
+											<Skeleton className="h-10 w-10" containerClassName="flex" />
+										</>
+									)
+								)}
+							</div>
+						</div>
+					</NavigationMenuPrimitive.List>
+				</NavigationMenuPrimitive.Root>
+			</NavigationWrapper>
+
+			<ul
+				className={classNames(
+					'fixed top-0 bottom-0 z-50 w-full bg-white transition-all duration-100',
+					isOpen ? 'right-0' : '-right-full'
+				)}
+			>
+				<div className="relative h-full w-full">
+					<div className="absolute top-4 right-4">
+						{user ? (
+							<ProfileDropdown user={user} signOutMutation={signOutMutation} />
+						) : (
+							isUserLoading && <Skeleton className="h-10 w-10" />
+						)}
+					</div>
+
+					<div className="flex h-full w-full flex-col items-center justify-center">
+						<button
+							onClick={() => {
+								setIsOpen(false);
+							}}
+						>
+							<FontAwesomeIcon
+								fill="currentColor"
+								className="mb-3 h-8 w-8 cursor-pointer"
+								size="2x"
+								icon={faXmark}
+							/>
+						</button>
+
+						{!user && !isUserLoading && (
+							<FullscreenLinkItem
+								index={0}
+								link={`/auth/signin`}
+								label={'Sign In'}
+								onClick={() => {
+									setIsOpen(false);
+								}}
+							/>
+						)}
+
+						{user ? (
+							<FullscreenLinkItem
+								index={0}
+								link={`/events/attending`}
+								label={'Events'}
+								onClick={() => {
+									setIsOpen(false);
+								}}
+							/>
+						) : (
+							<FullscreenLinkItem
+								index={0}
+								link={`/events`}
+								label={'Events'}
+								onClick={() => {
+									setIsOpen(false);
+								}}
+							/>
+						)}
+
+						<FullscreenLinkItem
+							index={1}
+							link={`/pricing`}
+							label={'Pricing'}
+							onClick={() => {
+								setIsOpen(false);
+							}}
+						/>
+						<FullscreenLinkItem
+							index={2}
+							link={`/contact`}
+							label={'Contact'}
+							onClick={() => {
+								setIsOpen(false);
+							}}
+						/>
+						<FullscreenLinkItem
+							index={3}
+							link={`/guides`}
+							label={'Guides'}
+							onClick={() => {
+								setIsOpen(false);
+							}}
+						/>
+						<FullscreenLinkItem
+							index={4}
+							link={`/`}
+							label={'Home'}
+							onClick={() => {
+								setIsOpen(false);
+							}}
+						/>
+					</div>
+				</div>
+			</ul>
 		</>
 	);
 };
