a05d892b2d5fc84d7a2df9c77dc8a06b3d65733e
Jack Kelly <kcjackkelly@gmail.com>
2022-06-30 14:20:21 -0500

    card grid component

---
+++ components/cards/CardGrid.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+type CardGridProps = {};
+
+export const CardGrid: React.FC<CardGridProps> = (props) => {
+	const { children } = props;
+
+	return <div className="grid grid-cols-1 md:grid-cols-2 gap-8">{children}</div>;
+};
