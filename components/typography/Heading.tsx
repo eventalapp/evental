8ef5ca31599246d02387b2c8f5fc47cd7d9649c8
Jack Kelly <kcjackkelly@gmail.com>
2022-06-30 11:07:40 -0500

    heading component variants and level 4

--- components/typography/Heading.tsx
+++ components/typography/Heading.tsx
@@ -2,11 +2,14 @@ import classNames from 'classnames';
 import React from 'react';
 
 const variants = {
+	'sm': 'text-sm font-bold leading-[1.2] tracking-tight md:text-base',
+	'xl': 'text-xl font-bold leading-[1.2] tracking-tight md:text-2xl',
 	'default': 'text-2xl font-bold leading-[1.2] tracking-tight md:text-3xl',
+	'3xl': 'text-3xl font-black leading-tight tracking-tight sm:text-4xl',
 	'4xl': 'text-4xl font-black leading-tight tracking-tight sm:text-5xl lg:text-6xl'
 };
 
-type Props = { className?: string; level?: 1 | 2 | 3 | 5 | 6; variant?: keyof typeof variants };
+type Props = { className?: string; level?: 1 | 2 | 3 | 4 | 5 | 6; variant?: keyof typeof variants };
 
 type HeadingTag = 'h1' | 'h2' | 'h3' | 'h4' | 'h5' | 'h6';
 
