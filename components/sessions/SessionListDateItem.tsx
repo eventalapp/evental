65f51c3fa1c3b258d8486f41b9351d3863cb1eba
Jack Kelly <kcjackkelly@gmail.com>
2022-06-30 22:31:20 -0500

    session list date item

---
+++ components/sessions/SessionListDateItem.tsx
@@ -0,0 +1,37 @@
+import Prisma from '@prisma/client';
+import dayjs from 'dayjs';
+import React from 'react';
+
+import { SessionWithVenue } from '../../pages/api/events/[eid]/sessions';
+import { SessionListHourItem } from './SessionListHourItem';
+
+type SessionListDateItemProps = {
+	date: string;
+	sessionsByHour: Record<string, SessionWithVenue[]>;
+	admin?: boolean;
+	event: Prisma.Event;
+};
+
+export const SessionListDateItem: React.FC<SessionListDateItemProps> = (props) => {
+	const { date, sessionsByHour, event, admin } = props;
+
+	return (
+		<div key={date}>
+			<p className="inline-block pb-0.5 text-xl text-gray-700">
+				<span className="font-medium text-gray-900">{dayjs(date).format('dddd')}</span>,{' '}
+				{dayjs(date).format('MMMM D')}
+			</p>
+			{Object.entries(sessionsByHour).map(([hour, sessions]) => {
+				return (
+					<SessionListHourItem
+						key={hour}
+						sessions={sessions}
+						admin={admin}
+						event={event}
+						hour={hour}
+					/>
+				);
+			})}
+		</div>
+	);
+};
