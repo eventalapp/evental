75198a92f5a6fd8cf07cea35d9f31b55ea16dfaf
Jack Kelly <kcjackkelly@gmail.com>
2022-07-04 15:22:29 -0500

    icon button tooltip refactor with colors

--- components/IconButtonTooltip.tsx
+++ components/IconButtonTooltip.tsx
@@ -2,9 +2,15 @@ import { IconDefinition } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import classNames from 'classnames';
 import React from 'react';
+import Skeleton from 'react-loading-skeleton';
 
-import { faSpinnerThird } from '../icons';
 import Tooltip from './radix/components/Tooltip';
+
+export const iconColors = {
+	gray: 'text-gray-700 hover:text-gray-600',
+	red: 'text-red-500 hover:text-red-400',
+	none: ''
+};
 
 type Props = React.FC<
 	{
@@ -13,30 +19,37 @@ type Props = React.FC<
 		side?: 'bottom' | 'top' | 'right' | 'left' | undefined;
 		LoadingComponent?: JSX.Element;
 		isLoading?: boolean;
+		color?: keyof typeof iconColors;
 	} & React.DetailedHTMLProps<React.ButtonHTMLAttributes<HTMLButtonElement>, HTMLButtonElement>
 >;
 
 export const IconButtonTooltip: Props = (props) => {
-	const { children, icon, message, className, side, isLoading, ...rest } = props;
+	const {
+		children,
+		icon,
+		message,
+		className,
+		side = 'top',
+		isLoading,
+		color = 'gray',
+		...rest
+	} = props;
 
 	return (
 		<Tooltip message={message} side={side}>
-			<div
-				className={classNames(
-					'-mr-1 inline-block disabled:cursor-not-allowed disabled:text-gray-300',
-					className
-				)}
-			>
-				<button {...rest}>
+			<div>
+				<button
+					className={classNames(
+						'flex items-center disabled:cursor-not-allowed disabled:opacity-50 justify-center',
+						iconColors[color],
+						className
+					)}
+					{...rest}
+				>
 					{isLoading ? (
-						<FontAwesomeIcon
-							fill="currentColor"
-							className="h-6 w-6 animate-spin p-1"
-							size="1x"
-							icon={faSpinnerThird}
-						/>
+						<Skeleton className="w-5 h-5 p-1" />
 					) : (
-						<FontAwesomeIcon fill="currentColor" className="h-6 w-6 p-1" size="1x" icon={icon} />
+						<FontAwesomeIcon fill="currentColor" className="h-5 w-5 p-1" size="1x" icon={icon} />
 					)}
 				</button>
 			</div>
