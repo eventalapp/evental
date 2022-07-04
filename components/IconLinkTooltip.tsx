812b6adfbd5a07442bcddeadafd34964036767f1
Jack Kelly <kcjackkelly@gmail.com>
2022-07-04 14:48:05 -0500

    revert icon link tooltip changes

--- components/IconLinkTooltip.tsx
+++ components/IconLinkTooltip.tsx
@@ -8,7 +8,7 @@ import Tooltip from './radix/components/Tooltip';
 
 type Props = React.FC<
 	{
-		href?: string;
+		href: string;
 		icon: IconDefinition;
 		message: string;
 		side?: 'bottom' | 'top' | 'right' | 'left' | undefined;
@@ -18,20 +18,6 @@ type Props = React.FC<
 
 export const IconLinkTooltip: Props = (props) => {
 	const { children, href, icon, message, className, side, ...rest } = props;
-
-	if (href) {
-		return (
-			<Tooltip message={message} side={side}>
-				<div className="-mr-1">
-					<Link href={href} passHref>
-						<a className={classNames('flex items-center justify-center', className)} {...rest}>
-							<FontAwesomeIcon fill="currentColor" className="h-5 w-5 p-1" size="1x" icon={icon} />
-						</a>
-					</Link>
-				</div>
-			</Tooltip>
-		);
-	}
 
 	return (
 		<Tooltip message={message} side={side}>
