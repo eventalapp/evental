21198a89661b445f578683149e180a05c4b92c45
Jack Kelly <kcjackkelly@gmail.com>
2022-07-04 15:22:06 -0500

    DeleteVenueDialog.tsx

---
+++ components/radix/components/DeleteVenueDialog.tsx
@@ -0,0 +1,83 @@
+import { zodResolver } from '@hookform/resolvers/zod';
+import * as DialogPrimitive from '@radix-ui/react-alert-dialog';
+import React, { useEffect, useState } from 'react';
+import { useForm } from 'react-hook-form';
+
+import { useDeleteVenueMutation } from '../../../hooks/mutations/useDeleteVenueMutation';
+import { DeleteDataPayload, DeleteDataSchema } from '../../../utils/schemas';
+import { LoadingInner } from '../../error/LoadingInner';
+import { Button } from '../../form/Button';
+import { ErrorMessage } from '../../form/ErrorMessage';
+import { Input } from '../../form/Input';
+import { Label } from '../../form/Label';
+import { DialogContent } from './DialogContent';
+
+interface Props {
+	eid: string;
+	vid: string;
+}
+
+const DeleteVenueDialog: React.FC<Props> = (props) => {
+	const { eid, vid, children } = props;
+	let [isOpen, setIsOpen] = useState(false);
+	const { deleteVenueMutation } = useDeleteVenueMutation(String(eid), String(vid));
+	const {
+		register,
+		handleSubmit,
+		formState: { errors }
+	} = useForm<DeleteDataPayload>({
+		resolver: zodResolver(DeleteDataSchema)
+	});
+
+	useEffect(() => {
+		if (!deleteVenueMutation.isLoading && deleteVenueMutation.isSuccess) {
+			setIsOpen(false);
+		}
+	}, [deleteVenueMutation.isLoading]);
+
+	return (
+		<DialogPrimitive.Root open={isOpen} onOpenChange={setIsOpen}>
+			<DialogPrimitive.Trigger type="button" asChild>
+				{children}
+			</DialogPrimitive.Trigger>
+
+			<DialogContent isOpen={isOpen} setIsOpen={setIsOpen}>
+				<DialogPrimitive.Title className="text-xl font-bold text-gray-900 dark:text-gray-100">
+					Delete this venue?
+				</DialogPrimitive.Title>
+				<DialogPrimitive.Description className="mt-2 text-sm font-normal text-gray-700 dark:text-gray-400">
+					After this venue is deleted, all data regarding this venue will be deleted (Including all
+					sessions under this venue).
+				</DialogPrimitive.Description>
+
+				<form
+					onSubmit={handleSubmit(() => {
+						deleteVenueMutation.mutate();
+					})}
+				>
+					<div className="mt-4">
+						<Label htmlFor="confirmDelete">Confirm *</Label>
+						<Input
+							autoComplete="off"
+							placeholder={`Type "Delete" to confirm`}
+							{...register('confirm')}
+							autoFocus={true}
+						/>
+						{errors.confirm?.message && <ErrorMessage>{errors.confirm?.message}</ErrorMessage>}
+					</div>
+					<div className="mt-4 flex justify-end space-x-2">
+						<DialogPrimitive.Cancel asChild>
+							<Button variant="no-bg">Cancel</Button>
+						</DialogPrimitive.Cancel>
+
+						<Button variant="danger" disabled={deleteVenueMutation.isLoading}>
+							{deleteVenueMutation.isLoading ? <LoadingInner /> : 'Delete'}
+						</Button>
+					</div>
+				</form>
+			</DialogContent>
+		</DialogPrimitive.Root>
+	);
+};
+
+export default DeleteVenueDialog;
