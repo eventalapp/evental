6b63d134665d12e2da8870c75ac8a81e014d62bf
Jack Kelly <kcjackkelly@gmail.com>
2022-07-05 14:10:51 -0500

    loading skeleton impl for SessionWithEventList.tsx

--- components/sessions/SessionWithEventList.tsx
+++ components/sessions/SessionWithEventList.tsx
@@ -9,6 +9,7 @@ import { SessionWithVenueEvent } from '../../pages/api/users/[uid]/sessions';
 import { sessionWithEventListReducer } from '../../utils/reducer';
 import { NotFound } from '../error/NotFound';
 import { SessionHoverCard } from './SessionHoverCard';
+import { sessionListSkeleton } from './SessionList';
 
 type Props = {
 	admin?: boolean;
@@ -23,180 +24,193 @@ export const SessionWithEventList: React.FC<Props> = (props) => {
 		return <NotFound message="No sessions found." />;
 	}
 
-	if (!sessions) return null;
-	// TODO: Componetize this and impl react skeleton
-
-	const previousSessions = sessions.filter((session) =>
-		dayjs(session.endDate).isBefore(new Date())
-	);
-	const upcomingSessions = sessions.filter((session) => dayjs(session.endDate).isAfter(new Date()));
+	const previousSessions =
+		sessions && sessions.filter((session) => dayjs(session.endDate).isBefore(new Date()));
+	const upcomingSessions =
+		sessions && sessions.filter((session) => dayjs(session.endDate).isAfter(new Date()));
 
 	return (
 		<div className="relative min-h-[25px]">
-			{previousSessions && previousSessions.length >= 1 && (
-				<button
-					className="absolute top-0 right-0 z-20 cursor-pointer text-sm text-gray-500 flex flex-row items-center"
-					onClick={() => {
-						setShowPastEvents(!showPastEvents);
-					}}
-				>
-					{showPastEvents ? (
-						<FontAwesomeIcon fill="currentColor" className="mr-1.5 h-4 w-4" icon={faEyeSlash} />
-					) : (
-						<FontAwesomeIcon fill="currentColor" className="mr-1.5 h-4 w-4" icon={faEye} />
+			{sessions ? (
+				<>
+					{' '}
+					{previousSessions && previousSessions.length >= 1 && (
+						<button
+							className="absolute top-0 right-0 z-20 cursor-pointer text-sm text-gray-500 flex flex-row items-center"
+							onClick={() => {
+								setShowPastEvents(!showPastEvents);
+							}}
+						>
+							{showPastEvents ? (
+								<FontAwesomeIcon fill="currentColor" className="mr-1.5 h-4 w-4" icon={faEyeSlash} />
+							) : (
+								<FontAwesomeIcon fill="currentColor" className="mr-1.5 h-4 w-4" icon={faEye} />
+							)}
+							{showPastEvents ? 'Hide' : 'Show'} past sessions
+						</button>
 					)}
-					{showPastEvents ? 'Hide' : 'Show'} past sessions
-				</button>
-			)}
-
-			<div
-				className={classNames('overflow-hidden transition-all', showPastEvents ? 'h-auto' : 'h-0')}
-			>
-				{Object.entries(previousSessions.reduce(sessionWithEventListReducer, {})).map(
-					([date, hourObject]) => {
-						return (
-							<div key={date}>
-								<p className="mt-2 inline-block px-2 pb-0.5 text-xl">
-									{dayjs(date).format('dddd, MMM D')}
-								</p>
-								{Object.entries(hourObject).map(([hour, sessions]) => {
+					<div
+						className={classNames(
+							'overflow-hidden transition-all',
+							showPastEvents ? 'h-auto' : 'h-0'
+						)}
+					>
+						{previousSessions &&
+							Object.entries(previousSessions.reduce(sessionWithEventListReducer, {})).map(
+								([date, hourObject]) => {
 									return (
-										<div className="flex flex-row" key={hour}>
-											<span className="w-24 py-2 pr-3 text-right text-sm text-gray-700">
-												{dayjs(hour).format('h:mm A z')}
-											</span>
-											<div className="w-full">
-												{sessions.map((session) => {
-													if (!session.event) {
-														return null;
-													}
-
-													return (
-														<SessionHoverCard
-															admin={admin}
-															session={session}
-															event={session.event}
-															key={session.id}
-															redirect={false}
-														>
-															<div className="mr-2 mb-2 inline-block">
-																<Link
-																	href={`/events/${session.event.slug}${
-																		admin ? '/admin' : ''
-																	}/sessions/${session.slug}`}
-																>
-																	<a className="inline-block">
-																		<div className="flex flex-row rounded-md transition-all duration-100 hover:bg-gray-50">
-																			<div className="flex grow flex-row flex-wrap justify-between py-2 px-3">
-																				<div className="flex flex-row items-center justify-between">
-																					<div
-																						className="mr-3 h-4 w-4 rounded-full"
-																						style={{
-																							backgroundColor: session?.category?.color ?? '#888888'
-																						}}
-																					/>
-																					<div>
-																						<span className="block text-lg leading-tight">
-																							{session.name}
-																						</span>{' '}
-																						{session.category?.name ? (
-																							<span className="text-sm text-gray-500">
-																								{session.category?.name}
-																							</span>
-																						) : (
-																							<em className="text-sm text-gray-500">{'No Type'}</em>
-																						)}
-																					</div>
-																				</div>
-																			</div>
-																		</div>
-																	</a>
-																</Link>
-															</div>
-														</SessionHoverCard>
-													);
-												})}
-											</div>
-										</div>
-									);
-								})}
-							</div>
-						);
-					}
-				)}
-			</div>
-
-			{Object.entries(upcomingSessions.reduce(sessionWithEventListReducer, {})).map(
-				([date, hourObject]) => {
-					return (
-						<div key={date}>
-							<p className="mt-2 inline-block px-2 pb-0.5 text-xl">
-								{dayjs(date).format('dddd, MMM D')}
-							</p>
-							{Object.entries(hourObject).map(([hour, sessions]) => {
-								return (
-									<div className="flex flex-row" key={hour}>
-										<span className="w-24 py-2 pr-3 text-right text-sm text-gray-700">
-											{dayjs(hour).format('h:mm A z')}
-										</span>
-										<div className="w-full">
-											{sessions.map((session) => {
-												if (!session.event) {
-													return null;
-												}
-
+										<div key={date}>
+											<p className="mt-2 inline-block px-2 pb-0.5 text-xl">
+												{dayjs(date).format('dddd, MMM D')}
+											</p>
+											{Object.entries(hourObject).map(([hour, sessions]) => {
 												return (
-													<SessionHoverCard
-														admin={admin}
-														session={session}
-														event={session.event}
-														key={session.id}
-													>
-														<div className="mr-2 mb-2 inline-block">
-															<Link
-																href={`/events/${session.event.slug}${
-																	admin ? '/admin' : ''
-																}/sessions/${session.slug}`}
-															>
-																<a className="inline-block">
-																	<div className="flex flex-row rounded-md transition-all duration-100 hover:bg-gray-50">
-																		<div className="flex grow flex-row flex-wrap justify-between py-2 px-3">
-																			<div className="flex flex-row items-center justify-between">
-																				<div
-																					className="mr-3 h-4 w-4 rounded-full"
-																					style={{
-																						backgroundColor: session?.category?.color ?? '#888888'
-																					}}
-																				/>
-																				<div>
-																					<span className="block text-lg leading-tight">
-																						{session.name}
-																					</span>
+													<div className="flex flex-row" key={hour}>
+														<span className="w-24 py-2 pr-3 text-right text-sm text-gray-700">
+															{dayjs(hour).format('h:mm A z')}
+														</span>
+														<div className="w-full">
+															{sessions.map((session) => {
+																if (!session.event) {
+																	return null;
+																}
 
-																					{session.category?.name ? (
-																						<span className="text-sm text-gray-500">
-																							{session.category?.name}
-																						</span>
-																					) : (
-																						<em className="text-sm text-gray-500">{'No Type'}</em>
-																					)}
-																				</div>
-																			</div>
+																return (
+																	<SessionHoverCard
+																		admin={admin}
+																		session={session}
+																		event={session.event}
+																		key={session.id}
+																		redirect={false}
+																	>
+																		<div className="mr-2 mb-2 inline-block">
+																			<Link
+																				href={`/events/${session.event.slug}${
+																					admin ? '/admin' : ''
+																				}/sessions/${session.slug}`}
+																			>
+																				<a className="inline-block">
+																					<div className="flex flex-row rounded-md transition-all duration-100 hover:bg-gray-50">
+																						<div className="flex grow flex-row flex-wrap justify-between py-2 px-3">
+																							<div className="flex flex-row items-center justify-between">
+																								<div
+																									className="mr-3 h-4 w-4 rounded-full"
+																									style={{
+																										backgroundColor:
+																											session?.category?.color ?? '#888888'
+																									}}
+																								/>
+																								<div>
+																									<span className="block text-lg leading-tight">
+																										{session.name}
+																									</span>{' '}
+																									{session.category?.name ? (
+																										<span className="text-sm text-gray-500">
+																											{session.category?.name}
+																										</span>
+																									) : (
+																										<em className="text-sm text-gray-500">
+																											{'No Type'}
+																										</em>
+																									)}
+																								</div>
+																							</div>
+																						</div>
+																					</div>
+																				</a>
+																			</Link>
 																		</div>
-																	</div>
-																</a>
-															</Link>
+																	</SessionHoverCard>
+																);
+															})}
 														</div>
-													</SessionHoverCard>
+													</div>
 												);
 											})}
 										</div>
+									);
+								}
+							)}
+					</div>
+					{upcomingSessions &&
+						Object.entries(upcomingSessions.reduce(sessionWithEventListReducer, {})).map(
+							([date, hourObject]) => {
+								return (
+									<div key={date}>
+										<p className="mt-2 inline-block px-2 pb-0.5 text-xl">
+											{dayjs(date).format('dddd, MMM D')}
+										</p>
+										{Object.entries(hourObject).map(([hour, sessions]) => {
+											return (
+												<div className="flex flex-row" key={hour}>
+													<span className="w-24 py-2 pr-3 text-right text-sm text-gray-700">
+														{dayjs(hour).format('h:mm A z')}
+													</span>
+													<div className="w-full">
+														{sessions.map((session) => {
+															if (!session.event) {
+																return null;
+															}
+
+															return (
+																<SessionHoverCard
+																	admin={admin}
+																	session={session}
+																	event={session.event}
+																	key={session.id}
+																>
+																	<div className="mr-2 mb-2 inline-block">
+																		<Link
+																			href={`/events/${session.event.slug}${
+																				admin ? '/admin' : ''
+																			}/sessions/${session.slug}`}
+																		>
+																			<a className="inline-block">
+																				<div className="flex flex-row rounded-md transition-all duration-100 hover:bg-gray-50">
+																					<div className="flex grow flex-row flex-wrap justify-between py-2 px-3">
+																						<div className="flex flex-row items-center justify-between">
+																							<div
+																								className="mr-3 h-4 w-4 rounded-full"
+																								style={{
+																									backgroundColor:
+																										session?.category?.color ?? '#888888'
+																								}}
+																							/>
+																							<div>
+																								<span className="block text-lg leading-tight">
+																									{session.name}
+																								</span>
+
+																								{session.category?.name ? (
+																									<span className="text-sm text-gray-500">
+																										{session.category?.name}
+																									</span>
+																								) : (
+																									<em className="text-sm text-gray-500">
+																										{'No Type'}
+																									</em>
+																								)}
+																							</div>
+																						</div>
+																					</div>
+																				</div>
+																			</a>
+																		</Link>
+																	</div>
+																</SessionHoverCard>
+															);
+														})}
+													</div>
+												</div>
+											);
+										})}
 									</div>
 								);
-							})}
-						</div>
-					);
-				}
+							}
+						)}
+				</>
+			) : (
+				sessionListSkeleton
 			)}
 		</div>
 	);
