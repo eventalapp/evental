821dbb941e732b17e2863a6397eba98af29d7c7e
Jack Kelly <kcjackkelly@gmail.com>
2022-07-05 13:59:11 -0500

    if no user on organizer invite, sign up prompt

--- pages/events/[eid]/invites/organizer.tsx
+++ pages/events/[eid]/invites/organizer.tsx
@@ -1,9 +1,9 @@
 import { NextPage } from 'next';
 import { NextSeo } from 'next-seo';
+import Link from 'next/link';
 import { useRouter } from 'next/router';
 import React from 'react';
 
-import { UnauthorizedPage } from '../../../../components/error/UnauthorizedPage';
 import { ViewErrorPage } from '../../../../components/error/ViewErrorPage';
 import Column from '../../../../components/layout/Column';
 import { FlexRowBetween } from '../../../../components/layout/FlexRowBetween';
@@ -12,6 +12,7 @@ import PageWrapper from '../../../../components/layout/PageWrapper';
 import { Navigation } from '../../../../components/navigation';
 import { Button } from '../../../../components/primitives/Button';
 import { Heading } from '../../../../components/primitives/Heading';
+import { LinkButton } from '../../../../components/primitives/LinkButton';
 import { useAcceptOrganizerInviteMutation } from '../../../../hooks/mutations/useAcceptOrganizerInviteMutation';
 import { useEventQuery } from '../../../../hooks/queries/useEventQuery';
 import { useUser } from '../../../../hooks/queries/useUser';
@@ -19,32 +20,74 @@ import { AcceptOrganizerInviteSchema } from '../../../../utils/schemas';
 
 const OrganizerInvitePage: NextPage = () => {
 	const router = useRouter();
-	const { user, isUserLoading } = useUser();
+	const { user } = useUser();
 	const { eid, code } = router.query;
 	const { acceptOrganizerInviteMutation } = useAcceptOrganizerInviteMutation(String(eid));
 	const { event, eventError } = useEventQuery(String(eid));
+
+	const Seo = event && (
+		<NextSeo
+			title={`Accept Organizer Invite`}
+			additionalLinkTags={[
+				{
+					rel: 'icon',
+					href: `https://cdn.evental.app${event.image}`
+				}
+			]}
+		/>
+	);
 
 	if (eventError) {
 		return <ViewErrorPage errors={[eventError]} />;
 	}
 
-	if (!user && !isUserLoading) {
-		return <UnauthorizedPage />;
+	if (!user?.id) {
+		let params = new URLSearchParams();
+
+		params.append('redirectUrl', String(router.asPath));
+
+		return (
+			<PageWrapper>
+				{Seo}
+
+				<Navigation />
+
+				<Column variant="halfWidth">
+					<div className="space-y-5">
+						<Heading>Create an account</Heading>
+						<p className="text-gray-700">
+							To organize this event, please{' '}
+							<Link href={`/auth/signup?${params}`}>
+								<a className="text-gray-900 underline">create an account</a>
+							</Link>{' '}
+							or{' '}
+							<Link href={`/auth/signin?${params}`}>
+								<a className="text-gray-900 underline">sign in</a>
+							</Link>{' '}
+							with your existing account.
+						</p>
+						<div className="flex flex-row justify-end">
+							<Button type="button" variant="no-bg" className="mr-3" onClick={router.back}>
+								Cancel
+							</Button>
+
+							<Link href={`/auth/signin?${params}`} passHref>
+								<LinkButton padding="large" variant="primary">
+									Sign in
+								</LinkButton>
+							</Link>
+						</div>
+					</div>
+				</Column>
+
+				<Footer color={event?.color} />
+			</PageWrapper>
+		);
 	}
 
 	return (
 		<PageWrapper>
-			{event && (
-				<NextSeo
-					title={`Accept Organizer Invite`}
-					additionalLinkTags={[
-						{
-							rel: 'icon',
-							href: `https://cdn.evental.app${event.image}`
-						}
-					]}
-				/>
-			)}
+			{Seo}
 
 			<Navigation />
 
@@ -62,6 +105,7 @@ const OrganizerInvitePage: NextPage = () => {
 						Cancel
 					</Button>
 					<Button
+						variant="primary"
 						onClick={() => {
 							const data = AcceptOrganizerInviteSchema.parse({ code: String(code) });
 
