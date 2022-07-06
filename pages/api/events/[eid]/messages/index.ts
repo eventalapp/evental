1e4b64168240e3a08ca64732be300cdab35825f5
Jack Kelly <kcjackkelly@gmail.com>
2022-07-05 22:04:27 -0500

    get event messages api route

---
+++ pages/api/events/[eid]/messages/index.ts
@@ -0,0 +1,40 @@
+import type Prisma from '@prisma/client';
+import { NextkitError } from 'nextkit';
+
+import { prisma } from '../../../../../prisma/client';
+import { api } from '../../../../../utils/api';
+import { getEvent } from '../index';
+
+export default api({
+	async GET({ req }) {
+		const { eid } = req.query;
+
+		const event = await getEvent(String(eid));
+
+		if (!event) {
+			throw new NextkitError(404, 'Event not found');
+		}
+
+		return await getMessages(String(eid));
+	}
+});
+
+export const getMessages = async (eid: string): Promise<Prisma.EventMessage[] | null> => {
+	const event = await getEvent(String(eid));
+
+	if (!event) {
+		return null;
+	}
+
+	const messages = await prisma.eventMessage.findMany({
+		where: {
+			eventId: event.id
+		}
+	});
+
+	if (!messages || messages.length === 0) {
+		return [];
+	}
+
+	return messages;
+};
