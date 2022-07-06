2c7ab885368d80e259a88fcdc982eb406f70f1ff
Jack Kelly <kcjackkelly@gmail.com>
2022-07-06 15:15:50 -0500

    message by id/slug api route

---
+++ pages/api/events/[eid]/messages/[mid].ts
@@ -0,0 +1,41 @@
+import type Prisma from '@prisma/client';
+import { NextkitError } from 'nextkit';
+
+import { prisma } from '../../../../../prisma/client';
+import { api } from '../../../../../utils/api';
+import { getEvent } from '../index';
+
+export default api({
+	async GET({ req }) {
+		const { eid, mid } = req.query;
+
+		const message = await getMessage(String(eid), String(mid));
+
+		if (!message) {
+			throw new NextkitError(404, 'Message not found');
+		}
+
+		return message;
+	}
+});
+
+export const getMessage = async (eid: string, mid: string): Promise<Prisma.EventMessage | null> => {
+	const event = await getEvent(eid);
+
+	if (!event) {
+		return null;
+	}
+
+	const message = await prisma.eventMessage.findFirst({
+		where: {
+			eventId: event.id,
+			OR: [{ id: String(mid) }, { slug: String(mid) }]
+		}
+	});
+
+	if (!message) {
+		return null;
+	}
+
+	return message;
+};
