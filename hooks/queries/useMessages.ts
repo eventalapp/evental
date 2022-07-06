f822e5dbccb3808609e4eb220b6de6482344247d
Jack Kelly <kcjackkelly@gmail.com>
2022-07-06 15:01:55 -0500

    usemessages query

---
+++ hooks/queries/useMessages.ts
@@ -0,0 +1,48 @@
+import Prisma from '@prisma/client';
+import axios, { AxiosError } from 'axios';
+import { ErroredAPIResponse, SuccessAPIResponse } from 'nextkit';
+import { useState } from 'react';
+import { useQuery } from 'react-query';
+
+export interface UseMessagesQueryData {
+	messages: Prisma.EventMessage[] | undefined;
+	isMessagesLoading: boolean;
+	messagesError: ErroredAPIResponse | null;
+}
+
+export interface UseMessagesQueryOptions {
+	initialData?: Prisma.EventMessage[] | undefined;
+}
+
+export const useMessages = (
+	eid: string,
+	args: UseMessagesQueryOptions = {}
+): UseMessagesQueryData => {
+	const { initialData } = args;
+	const [error, setError] = useState<ErroredAPIResponse | null>(null);
+
+	const { data: messages, isLoading: isMessagesLoading } = useQuery<
+		Prisma.EventMessage[],
+		AxiosError<ErroredAPIResponse>
+	>(
+		['messages', eid],
+		async () => {
+			return await axios
+				.get<SuccessAPIResponse<Prisma.EventMessage[]>>(`/api/events/${eid}/messages`)
+				.then((res) => res.data.data);
+		},
+		{
+			retry: 0,
+			enabled: eid !== undefined && eid !== 'undefined',
+			onError: (error) => {
+				setError(error?.response?.data ?? null);
+			},
+			onSuccess: () => {
+				setError(null);
+			},
+			initialData
+		}
+	);
+
+	return { messages, isMessagesLoading, messagesError: error };
+};
