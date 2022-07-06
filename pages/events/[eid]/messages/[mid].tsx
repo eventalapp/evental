81f07d9c8938cd3a8300d7958d0968d5ae171dd2
Jack Kelly <kcjackkelly@gmail.com>
2022-07-06 15:16:04 -0500

    view message public page

---
+++ pages/events/[eid]/messages/[mid].tsx
@@ -0,0 +1,80 @@
+import type { NextPage } from 'next';
+import { NextSeo } from 'next-seo';
+import { useRouter } from 'next/router';
+import React from 'react';
+
+import { NotFoundPage } from '../../../../components/error/NotFoundPage';
+import { PrivatePage } from '../../../../components/error/PrivatePage';
+import { ViewErrorPage } from '../../../../components/error/ViewErrorPage';
+import { EventHeader } from '../../../../components/events/EventHeader';
+import { EventNavigation } from '../../../../components/events/Navigation';
+import Column from '../../../../components/layout/Column';
+import { Footer } from '../../../../components/layout/Footer';
+import PageWrapper from '../../../../components/layout/PageWrapper';
+import { ViewMessage } from '../../../../components/messages/ViewMessage';
+import { useEventQuery } from '../../../../hooks/queries/useEventQuery';
+import { useIsOrganizerQuery } from '../../../../hooks/queries/useIsOrganizerQuery';
+import { useMessage } from '../../../../hooks/queries/useMessage';
+
+const ViewAttendeePage: NextPage = () => {
+	const router = useRouter();
+	const { mid, eid } = router.query;
+	const { message, messageError } = useMessage(String(eid), String(mid));
+	const { isOrganizer, isOrganizerLoading } = useIsOrganizerQuery(String(eid));
+	const { event, eventError } = useEventQuery(String(eid));
+
+	if (messageError) {
+		return <NotFoundPage message="Message not found." />;
+	}
+
+	if (eventError) {
+		return <ViewErrorPage errors={[eventError]} />;
+	}
+
+	if (event && event.privacy === 'PRIVATE' && !isOrganizer && !isOrganizerLoading) {
+		return <PrivatePage />;
+	}
+
+	return (
+		<PageWrapper>
+			{message && event && (
+				<NextSeo
+					title={`${message.title} — ${event.name}`}
+					description={`View all of the members of the ${message.title} message.`}
+					additionalLinkTags={[
+						{
+							rel: 'icon',
+							href: `https://cdn.evental.app${event.image}`
+						}
+					]}
+					openGraph={{
+						url: `https://evental.app/events/${event.slug}/messages/${message.slug}`,
+						title: `${message.title} — ${event.name}`,
+						description: `View all of the members of the ${message.title} message.`,
+						images: [
+							{
+								url: `https://cdn.evental.app${event.image}`,
+								width: 300,
+								height: 300,
+								alt: `${event.name} Logo Alt`,
+								type: 'image/jpeg'
+							}
+						]
+					}}
+				/>
+			)}
+
+			<EventNavigation eid={String(eid)} />
+
+			<Column>
+				<EventHeader adminLink={`/messages/${mid}`} eid={String(eid)} />
+
+				<ViewMessage eid={String(eid)} mid={String(mid)} message={message} />
+			</Column>
+
+			<Footer color={event?.color} />
+		</PageWrapper>
+	);
+};
+
+export default ViewAttendeePage;
