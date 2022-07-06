4ed889ecdec2d399faa9915faeada4fc8ec6369b
Jack Kelly <kcjackkelly@gmail.com>
2022-07-06 13:46:56 -0500

    generate email message templates

---
+++ email/generateTemplates.ts
@@ -0,0 +1,66 @@
+import * as fs from 'fs';
+
+import { eventMessage } from './templates/eventMessage';
+
+export type GenerateTemplateArgs = {
+	templateName: string;
+	subjectPart: string;
+	textPart: string;
+	htmlPart: string;
+};
+
+export const generateTemplates = (args: GenerateTemplateArgs) => {
+	const { templateName, subjectPart, textPart, htmlPart } = args;
+
+	fs.mkdirSync(`./email/output/${templateName}`, { recursive: true });
+
+	const template = `{
+    "Template": {
+      "TemplateName": ${JSON.stringify(templateName)},
+      "SubjectPart": ${JSON.stringify(subjectPart)},
+      "TextPart": ${JSON.stringify(textPart)},
+      "HtmlPart": ${JSON.stringify(htmlPart)}
+    }
+  }`;
+
+	fs.writeFileSync(`./email/output/${templateName}/template.json`, template);
+
+	const getInsideDoubleCurly = (str: string) =>
+		str
+			.split('{{')
+			.filter((val) => val.includes('}}'))
+			.map((val) => val.substring(0, val.indexOf('}}')));
+
+	const argsText = new Set(getInsideDoubleCurly(htmlPart));
+
+	const argObject: Record<string, string> = {};
+
+	argsText.forEach((val) => {
+		argObject[val] = val;
+	});
+
+	const operation = `{
+		"Source": "messages@evental.app",
+		"Template": ${JSON.stringify(templateName)},
+		"Destination": {
+			"ToAddresses": [ "email@gmail.com" ]
+		},
+		"TemplateData": ${JSON.stringify(JSON.stringify(argObject))}
+  }`;
+
+	fs.writeFileSync(`./email/output/${templateName}/operation.json`, operation);
+};
+
+const templatesToGenerate = [eventMessage];
+
+templatesToGenerate.forEach((template) => {
+	generateTemplates(template);
+});
+
+// CLI CRUD
+
+// aws ses send-templated-email --cli-input-json file://operation.json
+
+// aws ses create-template --cli-input-json file://template.json
+
+// aws ses delete-template --template-name EventMessage
