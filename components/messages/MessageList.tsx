4d14db9cb01e96a9ee037617a806d7fdbb462abd
Jack Kelly <kcjackkelly@gmail.com>
2022-07-05 22:04:40 -0500

    MessageList.tsx

---
+++ components/messages/MessageList.tsx
@@ -0,0 +1,56 @@
+import { faChevronRight } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import Prisma from '@prisma/client';
+import classNames from 'classnames';
+import Link from 'next/link';
+import React from 'react';
+
+import { NotFound } from '../error/NotFound';
+import Tooltip from '../primitives/Tooltip';
+
+type Props = {
+	eid: string;
+	admin?: boolean;
+	messages: Prisma.EventMessage[];
+};
+
+export const MessageList: React.FC<Props> = (props) => {
+	const { eid, messages, admin = false } = props;
+
+	if (messages && messages.length === 0) {
+		return <NotFound message="No messages found." />;
+	}
+
+	if (!messages) return null;
+
+	return (
+		<div>
+			{messages.map((message, i) => (
+				<Link
+					href={`/events/${eid}${admin ? '/admin' : ''}/messages/${message.slug}`}
+					key={message.id}
+					passHref
+				>
+					<a>
+						<div className={classNames('border-gray-200', i !== messages.length - 1 && 'border-b')}>
+							<div className="-mx-3 flex flex-row flex-wrap items-center justify-between p-3 hover:bg-gray-75">
+								<span className="block text-lg font-medium text-gray-800">{message.title}</span>
+
+								<Tooltip side={'top'} message={`Click to view this message`}>
+									<div className="-m-2 flex items-center justify-center p-2">
+										<FontAwesomeIcon
+											fill="currentColor"
+											size="1x"
+											className="h-5 w-5 text-gray-400"
+											icon={faChevronRight}
+										/>
+									</div>
+								</Tooltip>
+							</div>
+						</div>
+					</a>
+				</Link>
+			))}
+		</div>
+	);
+};
