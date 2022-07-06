5ed69d9feaac12ef67bb9184b2851100097b4249
Jack Kelly <kcjackkelly@gmail.com>
2022-07-05 22:37:52 -0500

    messages admin page

--- pages/events/[eid]/admin/messages/index.tsx
+++ pages/events/[eid]/admin/messages/index.tsx
@@ -1,16 +1,21 @@
+import { faPaperPlane } from '@fortawesome/free-solid-svg-icons';
 import type { NextPage } from 'next';
 import Head from 'next/head';
 import { useRouter } from 'next/router';
 
 import { AdminPageWrapper } from '../../../../../components/layout/AdminPageWrapper';
 import Column from '../../../../../components/layout/Column';
+import { FlexRowBetween } from '../../../../../components/layout/FlexRowBetween';
 import PageWrapper from '../../../../../components/layout/PageWrapper';
 import { SidebarWrapper } from '../../../../../components/layout/SidebarWrapper';
 import { Heading } from '../../../../../components/primitives/Heading';
+import { IconLinkTooltip } from '../../../../../components/primitives/IconLinkTooltip';
+import { useMessagesQuery } from '../../../../../hooks/queries/useMessagesQuery';
 
 const MessagePage: NextPage = () => {
 	const router = useRouter();
 	const { eid } = router.query;
+	const {} = useMessagesQuery(String(eid));
 
 	return (
 		<AdminPageWrapper eid={String(eid)}>
@@ -20,8 +25,17 @@ const MessagePage: NextPage = () => {
 				</Head>
 
 				<SidebarWrapper eid={String(eid)}>
-					<Column>
-						<Heading>Messages</Heading>
+					<Column variant="noMargin">
+						<FlexRowBetween>
+							<Heading>Messages</Heading>
+
+							<IconLinkTooltip
+								href={`/events/${eid}/admin/messages/create`}
+								message="Send a message"
+								icon={faPaperPlane}
+								color="gray"
+							/>
+						</FlexRowBetween>
 					</Column>
 				</SidebarWrapper>
 			</PageWrapper>
