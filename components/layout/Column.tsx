604e755b37d6379950e6fbf359683727ad44c95d
Jack Kelly <kcjackkelly@gmail.com>
2022-07-06 21:33:24 -0500

    column no margin variant sizing

--- components/layout/Column.tsx
+++ components/layout/Column.tsx
@@ -13,7 +13,7 @@ type LayoutProps = Props &
 const variants = {
 	padding: 'w-11/12 lg:max-w-5xl lg:w-4/6 sm:w-5/6 m-auto md:p-5 py-7 md:py-14',
 	halfWidth: 'w-11/12 lg:max-w-4xl lg:w-1/2 2xl:w-4/12 sm:w-5/6 m-auto md:p-5 py-7 md:py-14',
-	noMargin: 'w-11/12 lg:max-w-5xl lg:w-4/6 sm:w-5/6 md:px-10 py-7 md:py-14'
+	noMargin: 'w-11/12 lg:max-w-4xl lg:w-5/6 sm:w-5/6 md:px-10 py-7 md:py-14'
 };
 
 const Column: React.FC<LayoutProps> = (props) => {
