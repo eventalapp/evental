718ae8fd0fa97afd19ef85a9594defc2f9c27b69
Jack Kelly <kcjackkelly@gmail.com>
2022-07-06 21:45:03 -0500

    console.error on failed email send

--- utils/email/index.ts
+++ utils/email/index.ts
@@ -6,6 +6,7 @@ export const sendEmail = (params: SESV2.SendEmailRequest): Promise<void> => {
 	return new Promise((resolve, reject) => {
 		SES.sendEmail(params, (err) => {
 			if (err) {
+				console.error(err);
 				reject('Failed to send email');
 			} else {
 				resolve();
@@ -18,6 +19,7 @@ export const sendBulkEmail = (params: SESV2.SendBulkEmailRequest): Promise<void>
 	return new Promise((resolve, reject) => {
 		SES.sendBulkEmail(params, (err) => {
 			if (err) {
+				console.error(err);
 				reject('Failed to send email');
 			} else {
 				resolve();
