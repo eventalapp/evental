a3666347fd0768434af0e48837e523c16c686ec4
Jack Kelly <kcjackkelly@gmail.com>
2022-07-13 16:46:49 -0500

    settings sidebar, return to home

--- components/layout/SettingsSidebar.tsx
+++ components/layout/SettingsSidebar.tsx
@@ -1,4 +1,3 @@
-import router from 'next/router';
 import React from 'react';
 
 import { SidebarLink } from './SidebarLink';
@@ -8,13 +7,8 @@ const separator = <div className="h-[1px] w-full my-3 bg-gray-300" />;
 export const SettingsSidebar: React.FC = () => {
 	return (
 		<aside className="w-52 p-2.5 h-full py-7 md:py-14 flex flex-col">
-			<SidebarLink
-				onClick={() => {
-					router.back();
-				}}
-				className="font-medium"
-			>
-				{'<-'} Back
+			<SidebarLink href="/" className="font-medium">
+				{'<-'} Home
 			</SidebarLink>
 
 			{separator}
