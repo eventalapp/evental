ab0aa4d29ff81567bf41f8ab513375bf4cf8a0b3
Jack Kelly <kcjackkelly@gmail.com>
2022-07-14 15:32:16 -0500

    google analytics impl

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -17,6 +17,27 @@ export default class Document extends NextDocument {
 				className="h-full"
 			>
 				<Head>
+					{process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS && (
+						<>
+							<script
+								async
+								src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
+							/>
+							<script
+								dangerouslySetInnerHTML={{
+									__html: `
+										window.dataLayer = window.dataLayer || [];
+										function gtag(){dataLayer.push(arguments);}
+										gtag('js', new Date());
+										gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
+											page_path: window.location.pathname,
+										});
+									`
+								}}
+							/>
+						</>
+					)}
+
 					<link rel="manifest" href="/manifest.json" />
 				</Head>
 				<body className="h-full text-gray-900">
