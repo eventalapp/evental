c9feb229fdacb48e1207a334c41b4d94ec512dc9
Jack Kelly <kcjackkelly@gmail.com>
2022-07-17 14:52:18 -0500

    disabled next transpile debug

--- web/next.config.js
+++ web/next.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 // see https://github.com/martpie/next-transpile-modules#readme
-const withTM = require('next-transpile-modules')(['@eventalapp/shared'], { debug: true });
+const withTM = require('next-transpile-modules')(['@eventalapp/shared']);
 
 module.exports = withTM({
 	images: {
