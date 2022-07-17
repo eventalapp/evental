caa6dc5fe9b6fab8c9108d65b4924f4377d55f8f
Jack Kelly <kcjackkelly@gmail.com>
2022-07-16 21:04:48 -0500

    [BIG REFACTOR] Import mobile app as monorepo
    Co-authored-by: maxichrome <maxichrome@users.noreply.github.com>

---
+++ mobile/App.tsx
@@ -0,0 +1,44 @@
+import NetInfo from '@react-native-community/netinfo';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { NavigationContainer } from '@react-navigation/native';
+import * as React from 'react';
+import { AppState, AppStateStatus, Platform } from 'react-native';
+import { StatusBar } from 'react-native';
+import { focusManager, onlineManager } from 'react-query';
+
+import { EventsScreen } from './src/screens/Events';
+import { SettingsScreen } from './src/screens/Settings';
+
+function onAppStateChange(status: AppStateStatus) {
+	if (Platform.OS !== 'web') {
+		focusManager.setFocused(status === 'active');
+	}
+}
+
+onlineManager.setEventListener((setOnline) => {
+	return NetInfo.addEventListener((state) => {
+		setOnline(!!state.isConnected);
+	});
+});
+
+const Tab = createBottomTabNavigator();
+
+export default function App() {
+	React.useEffect(() => {
+		const subscription = AppState.addEventListener('change', onAppStateChange);
+
+		return () => subscription.remove();
+	}, []);
+
+	return (
+		<NavigationContainer>
+			<StatusBar barStyle="dark-content" />
+
+			<Tab.Navigator>
+				<Tab.Screen name="Events" component={EventsScreen} />
+				<Tab.Screen name="Settings" component={SettingsScreen} />
+			</Tab.Navigator>
+		</NavigationContainer>
+	);
+}
+
---
+++ mobile/src/screens/Events.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+export function EventsScreen() {
+	return (
+		<View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+			<Text>Events!</Text>
+		</View>
+	);
+}
+
