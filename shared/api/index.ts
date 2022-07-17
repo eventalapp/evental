42d09a2e69f94e64589d0b8b09c67735ba47732d
Jack Kelly <kcjackkelly@gmail.com>
2022-07-17 16:51:50 -0500

    remove babel-plugin-inline-dotenv

--- shared/api/index.ts
+++ shared/api/index.ts
@@ -1,9 +1,7 @@
 import axios from 'axios';
 
 const baseURL =
-	(process.env && process.env.API_HOST) ||
-	(process.env && process.env.NEXT_PUBLIC_VERCEL_URL) ||
-	'http://localhost:5555/api';
+	process.env.API_HOST || process.env.NEXT_PUBLIC_VERCEL_URL || 'http://localhost:5555/api';
 
 console.log({ baseURL });
 
