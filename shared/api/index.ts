03d85efcee9b3cea9fd8c948feea4439aeed5517
Jack Kelly <kcjackkelly@gmail.com>
2022-07-17 22:00:42 -0500

    fix next public vercel url api usage

--- shared/api/index.ts
+++ shared/api/index.ts
@@ -2,7 +2,9 @@ import axios from 'axios';
 
 const baseURL =
 	(process.env && process.env.API_HOST) ||
-	(process.env && process.env.NEXT_PUBLIC_VERCEL_URL) ||
+	(process.env &&
+		process.env.NEXT_PUBLIC_VERCEL_URL &&
+		`https://${process.env.NEXT_PUBLIC_VERCEL_URL}/api`) ||
 	'http://localhost:5555/api';
 
 console.log({ baseURL });
