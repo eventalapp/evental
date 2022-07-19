08b2d9d70e98ec887f8637c8fe77c252ffb2f912
Jack Kelly <kcjackkelly@gmail.com>
2022-07-19 16:00:56 -0500

    refresh indicator for upcoming events

--- mobile/screens/Events.tsx
+++ mobile/screens/Events.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Button, FlatList, Image, Text, View } from 'react-native';
+import { Button, FlatList, Image, RefreshControl, ScrollView, Text, View } from 'react-native';
 import { useSafeAreaInsets } from 'react-native-safe-area-context';
 
 import { useUpcomingEvents } from '@eventalapp/shared/hooks/queries/useUpcomingEvents';
@@ -8,101 +8,114 @@ import { useRefreshOnFocus } from '../hooks/useRefreshOnFocus';
 
 export function EventsScreen(props) {
 	const { navigation } = props;
-	const { data: upcomingEvents, refetch: refetchUpcomingEvents } = useUpcomingEvents();
+	const { data: upcomingEvents, refetch: refetchUpcomingEvents, isLoading } = useUpcomingEvents();
 	useRefreshOnFocus(refetchUpcomingEvents);
 	const safeAreaInsets = useSafeAreaInsets();
 
 	return (
-		<FlatList
-			contentContainerStyle={{
-				flexDirection: 'column',
-				justifyContent: 'center',
-				paddingTop: safeAreaInsets.top + 28,
-				paddingBottom: 12,
-				paddingLeft: safeAreaInsets.left + 28,
-				paddingRight: safeAreaInsets.right + 28
-			}}
-			ListHeaderComponent={() => (
-				<Text
-					style={{
-						fontSize: 36,
-						fontWeight: 'bold',
-						marginBottom: 18
+		<ScrollView
+			refreshControl={
+				<RefreshControl
+					colors={['#000000']}
+					tintColor="#000000"
+					refreshing={isLoading}
+					onRefresh={() => {
+						refetchUpcomingEvents();
 					}}
-				>
-					All Events
-				</Text>
-			)}
-			data={upcomingEvents}
-			renderItem={({ item }) => (
-				<View
-					style={{
-						flexDirection: 'row',
-						justifyContent: 'flex-start',
-						alignItems: 'center',
-						backgroundColor: 'white',
-						paddingVertical: 12,
-						paddingStart: 12,
-						paddingEnd: 18,
-						borderRadius: 8,
-						flex: 1,
-						marginBottom: 12
-					}}
-				>
-					<Image
-						source={{ uri: `https://cdn.evental.app${item.image}`, width: 52, height: 52 }}
+				/>
+			}
+		>
+			<FlatList
+				contentContainerStyle={{
+					flexDirection: 'column',
+					justifyContent: 'center',
+					paddingTop: safeAreaInsets.top + 28,
+					paddingBottom: 12,
+					paddingLeft: safeAreaInsets.left + 28,
+					paddingRight: safeAreaInsets.right + 28
+				}}
+				ListHeaderComponent={() => (
+					<Text
 						style={{
-							backgroundColor: '#dedede',
-							borderRadius: 8,
-							marginEnd: 10
-						}}
-					/>
-
-					<View
-						style={{
-							flexDirection: 'column'
+							fontSize: 36,
+							fontWeight: 'bold',
+							marginBottom: 18
 						}}
 					>
+						All Events
+					</Text>
+				)}
+				data={upcomingEvents}
+				renderItem={({ item }) => (
+					<View
+						style={{
+							flexDirection: 'row',
+							justifyContent: 'flex-start',
+							alignItems: 'center',
+							backgroundColor: 'white',
+							paddingVertical: 12,
+							paddingStart: 12,
+							paddingEnd: 18,
+							borderRadius: 8,
+							flex: 1,
+							marginBottom: 12
+						}}
+					>
+						<Image
+							source={{ uri: `https://cdn.evental.app${item.image}`, width: 52, height: 52 }}
+							style={{
+								backgroundColor: '#dedede',
+								borderRadius: 8,
+								marginEnd: 10
+							}}
+						/>
+
 						<View
 							style={{
-								flexDirection: 'row'
+								flexDirection: 'column'
 							}}
 						>
-							<Text
+							<View
 								style={{
-									fontSize: 14,
-									marginEnd: 4,
-									textTransform: 'uppercase'
+									flexDirection: 'row'
 								}}
 							>
-								{item.category} &middot;{' '}
-								{new Date(item.startDate).toLocaleDateString([], {
-									month: 'short',
-									day: 'numeric'
-								})}{' '}
-								–{' '}
-								{new Date(item.endDate).toLocaleDateString([], {
-									month: 'short',
-									day: 'numeric'
-								})}
+								<Text
+									style={{
+										fontSize: 14,
+										marginEnd: 4,
+										textTransform: 'uppercase'
+									}}
+								>
+									{item.category} &middot;{' '}
+									{new Date(item.startDate).toLocaleDateString([], {
+										month: 'short',
+										day: 'numeric'
+									})}{' '}
+									–{' '}
+									{new Date(item.endDate).toLocaleDateString([], {
+										month: 'short',
+										day: 'numeric'
+									})}
+								</Text>
+							</View>
+
+							<Text
+								style={{
+									fontSize: 18,
+									fontWeight: 'bold'
+								}}
+							>
+								{item.name}
 							</Text>
 						</View>
-
-						<Text
-							style={{
-								fontSize: 18,
-								fontWeight: 'bold'
-							}}
-						>
-							{item.name}
-						</Text>
+						<Button
+							title="view"
+							onPress={() => navigation.navigate('ViewEvent', { eid: item.slug })}
+						></Button>
 					</View>
-					<Button
-						title="view"
-						onPress={() => navigation.navigate('ViewEvent', { eid: item.slug })}
-					></Button>
-				</View>
-			)}
-		/>
+				)}
+			/>
+		</ScrollView>
 	);
 }
