34492e61d605aae82b4fadde663e03ffb2c030d0
Jack Kelly <kcjackkelly@gmail.com>
2022-07-24 12:24:03 -0500

    edit notification preferences hook

---
+++ shared/hooks/mutations/useEditNotificationPreferences.ts
@@ -0,0 +1,40 @@
+import * as Prisma from '@prisma/client';
+import axios, { AxiosError } from 'axios';
+import { ErroredAPIResponse, SuccessAPIResponse } from 'nextkit';
+import { Alert } from 'react-native';
+import { UseMutationResult, useMutation, useQueryClient } from 'react-query';
+
+import { api } from '../../api';
+import { NotificationPreferencePayload } from '../../utils/schema';
+
+export const useEditNotificationPreferences = () => {
+	const queryClient = useQueryClient();
+
+	return useMutation<
+		Prisma.NotificationPreference,
+		AxiosError<ErroredAPIResponse, unknown>,
+		NotificationPreferencePayload
+	>(
+		async (data) => {
+			return await api
+				.put<SuccessAPIResponse<Prisma.NotificationPreference>>(`/user/notifications`, data)
+				.then((res) => res.data.data)
+				.catch((err: AxiosError<ErroredAPIResponse, any>) => {
+					throw err.response?.data;
+				});
+		},
+		{
+			onSuccess: () => {
+				Alert.alert(
+					'Preferences Updated',
+					'Your user notification preferences have succesfully been updated.',
+					[{ text: 'OK' }]
+				);
+				void queryClient.refetchQueries(['notification-preference']);
+			},
+			onError: (error) => {
+				Alert.alert('Error', error?.message, [{ text: 'OK' }]);
+			}
+		}
+	);
+};
