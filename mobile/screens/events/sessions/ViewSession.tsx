8a015cfcb4a1876266eb1daa76459c82a8e4f8c2
Jack Kelly <kcjackkelly@gmail.com>
2022-07-24 10:40:06 -0500

    extract another flatlist out of scrollview

--- mobile/screens/events/sessions/ViewSession.tsx
+++ mobile/screens/events/sessions/ViewSession.tsx
@@ -86,56 +86,59 @@ export function ViewSessionScreen({ route, navigation }) {
 								<Text>{session.description}</Text>
 							)}
 
-							<FlatList
-								contentContainerStyle={{
+							<View
+								style={{
 									flexDirection: 'row',
 									justifyContent: 'space-between',
 									paddingTop: 12,
 									paddingBottom: 12,
 									flex: 4
 								}}
-								data={session.roleMembers}
-								renderItem={({ item: roleMember }) => (
-									<Pressable
-										style={{
-											flexDirection: 'column',
-											justifyContent: 'center',
-											alignItems: 'center',
-											backgroundColor: 'white',
-											padding: 12,
-											borderRadius: 8
-										}}
-										onPress={() =>
-											navigation.navigate('ViewAttendee', {
-												eid: eid,
-												uid: roleMember.attendee.user.slug
-											})
-										}
-									>
-										<Image
-											source={{
-												uri: `https://cdn.evental.app${roleMember.attendee.user.image}`,
-												width: 52,
-												height: 52
-											}}
+							>
+								{session.roleMembers &&
+									session.roleMembers.map((roleMember) => (
+										<Pressable
 											style={{
-												backgroundColor: '#dedede',
-												borderRadius: 8,
-												marginBottom: 10
+												flexDirection: 'column',
+												justifyContent: 'center',
+												alignItems: 'center',
+												backgroundColor: 'white',
+												padding: 12,
+												borderRadius: 8
 											}}
-										/>
-
-										<Text
-											style={{
-												fontSize: 18,
-												fontWeight: 'bold'
-											}}
+											key={roleMember.id}
+											onPress={() =>
+												navigation.navigate('ViewAttendee', {
+													eid: eid,
+													uid: roleMember.attendee.user.slug
+												})
+											}
 										>
-											{roleMember.attendee.user.name}
-										</Text>
-									</Pressable>
-								)}
-							/>
+											<Image
+												source={{
+													uri: `https://cdn.evental.app${roleMember.attendee.user.image}`,
+													width: 52,
+													height: 52
+												}}
+												style={{
+													backgroundColor: '#dedede',
+													borderRadius: 8,
+													marginBottom: 10
+												}}
+											/>
+
+											<Text
+												style={{
+													fontSize: 18,
+													fontWeight: 'bold'
+												}}
+											>
+												{roleMember.attendee.user.name}
+											</Text>
+										</Pressable>
+									))}
+							</View>
+
 							<Text>{JSON.stringify(session)}</Text>
 						</View>
 					)}
