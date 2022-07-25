ea8eb94a66784dc7e155aea6e39417a30516e51e
Jack Kelly <kcjackkelly@gmail.com>
2022-07-24 22:08:05 -0500

    remove unnecessary fragments

--- mobile/screens/events/ViewEvent.tsx
+++ mobile/screens/events/ViewEvent.tsx
@@ -36,148 +36,146 @@ export const ViewEventScreen = (props: Props) => {
 	} = useVenues({ eid });
 
 	return (
-		<>
-			<ScrollView
-				refreshControl={
-					<RefreshControl
-						colors={['#000000']}
-						tintColor="#000000"
-						refreshing={
-							isEventRefetching ||
-							isSessionsRefetching ||
-							isSessionsLoading ||
-							isVenuesLoading ||
-							isEventLoading
-						}
-						onRefresh={() => {
-							refetchEvent();
-							refetchSessions();
-						}}
-					/>
-				}
+		<ScrollView
+			refreshControl={
+				<RefreshControl
+					colors={['#000000']}
+					tintColor="#000000"
+					refreshing={
+						isEventRefetching ||
+						isSessionsRefetching ||
+						isSessionsLoading ||
+						isVenuesLoading ||
+						isEventLoading
+					}
+					onRefresh={() => {
+						refetchEvent();
+						refetchSessions();
+					}}
+				/>
+			}
+		>
+			<View
+				style={{
+					flexDirection: 'column',
+					justifyContent: 'center',
+					paddingTop: safeAreaInsets.top + 28,
+					paddingLeft: safeAreaInsets.left + 28,
+					paddingRight: safeAreaInsets.right + 28
+				}}
 			>
-				<View
-					style={{
-						flexDirection: 'column',
-						justifyContent: 'center',
-						paddingTop: safeAreaInsets.top + 28,
-						paddingLeft: safeAreaInsets.left + 28,
-						paddingRight: safeAreaInsets.right + 28
-					}}
-				>
-					{event && (
-						<View>
-							<View
+				{event && (
+					<View>
+						<View
+							style={{
+								display: 'flex',
+								flexDirection: 'row',
+								alignItems: 'center',
+								marginBottom: 20
+							}}
+						>
+							<Image
+								source={{ uri: `https://cdn.evental.app${event.image}`, width: 52, height: 52 }}
 								style={{
-									display: 'flex',
-									flexDirection: 'row',
-									alignItems: 'center',
-									marginBottom: 20
-								}}
-							>
-								<Image
-									source={{ uri: `https://cdn.evental.app${event.image}`, width: 52, height: 52 }}
-									style={{
-										backgroundColor: '#dedede',
-										borderRadius: 8,
-										marginRight: 10
-									}}
-								/>
-								<View style={{ display: 'flex', flexDirection: 'column' }}>
-									<Text
-										style={{
-											fontSize: 24,
-											fontWeight: 'bold',
-											marginBottom: 4
-										}}
-									>
-										{event.name}
-									</Text>
-									<Text>
-										{new Date(event.startDate).toLocaleDateString([], {
-											month: 'short',
-											day: 'numeric'
-										})}{' '}
-										–{' '}
-										{new Date(event.endDate).toLocaleDateString([], {
-											month: 'short',
-											day: 'numeric'
-										})}
-									</Text>
-								</View>
-							</View>
-						</View>
-					)}
-				</View>
-				<View
-					style={{
-						flexDirection: 'column',
-						justifyContent: 'center',
-						paddingTop: 12,
-						paddingBottom: 12,
-						paddingLeft: safeAreaInsets.left + 28,
-						paddingRight: safeAreaInsets.right + 28
-					}}
-				>
-					{sessions &&
-						sessions.map((session) => (
-							<Pressable
-								key={session.id}
-								style={{
-									flexDirection: 'row',
-									justifyContent: 'flex-start',
-									alignItems: 'center',
-									backgroundColor: 'white',
-									paddingVertical: 12,
-									paddingStart: 12,
-									paddingEnd: 18,
+									backgroundColor: '#dedede',
 									borderRadius: 8,
-									flex: 1,
-									marginBottom: 12
+									marginRight: 10
 								}}
-								onPress={() => navigation.navigate('ViewSession', { eid, sid: session.id })}
-							>
-								<View
+							/>
+							<View style={{ display: 'flex', flexDirection: 'column' }}>
+								<Text
 									style={{
-										width: 20,
-										height: 20,
-										borderRadius: 100,
-										marginRight: 12,
-										backgroundColor: session?.category?.color ?? '#888888'
-									}}
-								/>
-
-								<View
-									style={{
-										flexDirection: 'column'
+										fontSize: 24,
+										fontWeight: 'bold',
+										marginBottom: 4
 									}}
 								>
-									<Text
-										style={{
-											fontSize: 18,
-											fontWeight: 'bold',
-											marginBottom: 4
-										}}
-									>
-										{session.name}
+									{event.name}
+								</Text>
+								<Text>
+									{new Date(event.startDate).toLocaleDateString([], {
+										month: 'short',
+										day: 'numeric'
+									})}{' '}
+									–{' '}
+									{new Date(event.endDate).toLocaleDateString([], {
+										month: 'short',
+										day: 'numeric'
+									})}
+								</Text>
+							</View>
+						</View>
+					</View>
+				)}
+			</View>
+			<View
+				style={{
+					flexDirection: 'column',
+					justifyContent: 'center',
+					paddingTop: 12,
+					paddingBottom: 12,
+					paddingLeft: safeAreaInsets.left + 28,
+					paddingRight: safeAreaInsets.right + 28
+				}}
+			>
+				{sessions &&
+					sessions.map((session) => (
+						<Pressable
+							key={session.id}
+							style={{
+								flexDirection: 'row',
+								justifyContent: 'flex-start',
+								alignItems: 'center',
+								backgroundColor: 'white',
+								paddingVertical: 12,
+								paddingStart: 12,
+								paddingEnd: 18,
+								borderRadius: 8,
+								flex: 1,
+								marginBottom: 12
+							}}
+							onPress={() => navigation.navigate('ViewSession', { eid, sid: session.id })}
+						>
+							<View
+								style={{
+									width: 20,
+									height: 20,
+									borderRadius: 100,
+									marginRight: 12,
+									backgroundColor: session?.category?.color ?? '#888888'
+								}}
+							/>
+
+							<View
+								style={{
+									flexDirection: 'column'
+								}}
+							>
+								<Text
+									style={{
+										fontSize: 18,
+										fontWeight: 'bold',
+										marginBottom: 4
+									}}
+								>
+									{session.name}
+								</Text>
+
+								{session.roleMembers.length >= 1 && (
+									<Text style={{}}>
+										{session.roleMembers
+											.map((member) => member.attendee.user.name)
+											.splice(0, 3)
+											.join(', ')}
 									</Text>
+								)}
+							</View>
+						</Pressable>
+					))}
 
-									{session.roleMembers.length >= 1 && (
-										<Text style={{}}>
-											{session.roleMembers
-												.map((member) => member.attendee.user.name)
-												.splice(0, 3)
-												.join(', ')}
-										</Text>
-									)}
-								</View>
-							</Pressable>
-						))}
-
-					{venues && <Text>{JSON.stringify(venues)}</Text>}
-					{event && <Text>{JSON.stringify(event)}</Text>}
-				</View>
-			</ScrollView>
-		</>
+				{venues && <Text>{JSON.stringify(venues)}</Text>}
+				{event && <Text>{JSON.stringify(event)}</Text>}
+			</View>
+		</ScrollView>
 	);
 };
--- mobile/screens/events/sessions/ViewSession.tsx
+++ mobile/screens/events/sessions/ViewSession.tsx
@@ -25,132 +25,130 @@ export const ViewSessionScreen = (props: Props) => {
 	} = useSession({ eid, sid });
 
 	return (
-		<>
-			<ScrollView
-				refreshControl={
-					<RefreshControl
-						colors={['#000000']}
-						tintColor="#000000"
-						refreshing={isSessionRefetching || isSessionLoading}
-						onRefresh={() => {
-							refetchSession();
-						}}
-					/>
-				}
+		<ScrollView
+			refreshControl={
+				<RefreshControl
+					colors={['#000000']}
+					tintColor="#000000"
+					refreshing={isSessionRefetching || isSessionLoading}
+					onRefresh={() => {
+						refetchSession();
+					}}
+				/>
+			}
+		>
+			<View
+				style={{
+					flexDirection: 'column',
+					alignItems: 'flex-start',
+					paddingTop: safeAreaInsets.top + 28,
+					paddingBottom: 12,
+					paddingLeft: safeAreaInsets.left + 28,
+					paddingRight: safeAreaInsets.right + 28
+				}}
 			>
-				<View
+				<Pressable
+					onPress={() => {
+						navigation.goBack();
+					}}
 					style={{
-						flexDirection: 'column',
-						alignItems: 'flex-start',
-						paddingTop: safeAreaInsets.top + 28,
-						paddingBottom: 12,
-						paddingLeft: safeAreaInsets.left + 28,
-						paddingRight: safeAreaInsets.right + 28
+						flexDirection: 'row',
+						alignItems: 'center',
+						justifyContent: 'flex-start',
+						marginBottom: 8
 					}}
 				>
-					<Pressable
-						onPress={() => {
-							navigation.goBack();
-						}}
-						style={{
-							flexDirection: 'row',
-							alignItems: 'center',
-							justifyContent: 'flex-start',
-							marginBottom: 8
-						}}
-					>
-						<FontAwesomeIcon icon={faArrowLeft} style={{ marginRight: 6 }} />
-						<Text>Back</Text>
-					</Pressable>
+					<FontAwesomeIcon icon={faArrowLeft} style={{ marginRight: 6 }} />
+					<Text>Back</Text>
+				</Pressable>
 
-					{session && (
-						<View>
-							<View
-								style={{
-									display: 'flex',
-									flexDirection: 'row',
-									alignItems: 'center',
-									marginBottom: 20
-								}}
-							>
-								<View style={{ display: 'flex', flexDirection: 'column' }}>
-									<Text
-										style={{
-											fontSize: 24,
-											fontWeight: 'bold',
-											marginBottom: 4
-										}}
-									>
-										{session.name}
-									</Text>
-									<Text>
-										{formatDateRange(new Date(session.startDate), new Date(session.endDate))}
-									</Text>
-								</View>
-							</View>
-
-							{session.description && session.description.length > 0 && (
-								<Text>{session.description}</Text>
-							)}
-
-							{session.roleMembers && session.roleMembers.length > 0 && (
-								<View
+				{session && (
+					<View>
+						<View
+							style={{
+								display: 'flex',
+								flexDirection: 'row',
+								alignItems: 'center',
+								marginBottom: 20
+							}}
+						>
+							<View style={{ display: 'flex', flexDirection: 'column' }}>
+								<Text
 									style={{
-										flexDirection: 'row',
-										justifyContent: 'space-between',
-										marginTop: 12,
-										marginBottom: 12
+										fontSize: 24,
+										fontWeight: 'bold',
+										marginBottom: 4
 									}}
 								>
-									{session.roleMembers.map((roleMember) => (
-										<Pressable
-											style={{
-												flexDirection: 'column',
-												justifyContent: 'center',
-												alignItems: 'center',
-												backgroundColor: 'white',
-												padding: 12,
-												borderRadius: 8
-											}}
-											key={roleMember.id}
-											onPress={() =>
-												navigation.navigate('ViewAttendee', {
-													eid: eid,
-													uid: roleMember.attendee.user.slug
-												})
-											}
-										>
-											<Image
-												source={{
-													uri: `https://cdn.evental.app${roleMember.attendee.user.image}`,
-													width: 52,
-													height: 52
-												}}
-												style={{
-													backgroundColor: '#dedede',
-													borderRadius: 8,
-													marginBottom: 10
-												}}
-											/>
-
-											<Text
-												style={{
-													fontSize: 18,
-													fontWeight: 'bold'
-												}}
-											>
-												{roleMember.attendee.user.name}
-											</Text>
-										</Pressable>
-									))}
-								</View>
-							)}
-
-							<Text>{JSON.stringify(session)}</Text>
+									{session.name}
+								</Text>
+								<Text>
+									{formatDateRange(new Date(session.startDate), new Date(session.endDate))}
+								</Text>
+							</View>
 						</View>
-					)}
-				</View>
-			</ScrollView>
-		</>
+
+						{session.description && session.description.length > 0 && (
+							<Text>{session.description}</Text>
+						)}
+
+						{session.roleMembers && session.roleMembers.length > 0 && (
+							<View
+								style={{
+									flexDirection: 'row',
+									justifyContent: 'space-between',
+									marginTop: 12,
+									marginBottom: 12
+								}}
+							>
+								{session.roleMembers.map((roleMember) => (
+									<Pressable
+										style={{
+											flexDirection: 'column',
+											justifyContent: 'center',
+											alignItems: 'center',
+											backgroundColor: 'white',
+											padding: 12,
+											borderRadius: 8
+										}}
+										key={roleMember.id}
+										onPress={() =>
+											navigation.navigate('ViewAttendee', {
+												eid: eid,
+												uid: roleMember.attendee.user.slug
+											})
+										}
+									>
+										<Image
+											source={{
+												uri: `https://cdn.evental.app${roleMember.attendee.user.image}`,
+												width: 52,
+												height: 52
+											}}
+											style={{
+												backgroundColor: '#dedede',
+												borderRadius: 8,
+												marginBottom: 10
+											}}
+										/>
+
+										<Text
+											style={{
+												fontSize: 18,
+												fontWeight: 'bold'
+											}}
+										>
+											{roleMember.attendee.user.name}
+										</Text>
+									</Pressable>
+								))}
+							</View>
+						)}
+
+						<Text>{JSON.stringify(session)}</Text>
+					</View>
+				)}
+			</View>
+		</ScrollView>
 	);
 };
