e75b21dd92f2b0839d4a0f9d520b54ac671aa150
Jack Kelly <kcjackkelly@gmail.com>
2022-07-25 18:08:33 -0500

    remove colors export in shared const

--- shared/utils/const.ts
+++ shared/utils/const.ts
@@ -40,21 +40,6 @@ export const copy = {
 	}
 };
 
-export const colors = [
-	'#ff3b91',
-	'#d74bff',
-	'#ffc300',
-	'#ff46eb',
-	'#97c522',
-	'#ffab00',
-	'#ff3b3b',
-	'#12d9d9',
-	'#7c92ff',
-	'#ff9433',
-	'#31fa45',
-	'#a577ff'
-];
-
 export const timeZoneList = [
 	'Europe/Andorra',
 	'Asia/Dubai',
