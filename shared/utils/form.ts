cef92228eadaaeefbed57b94108dc760c86eb6c7
Jack Kelly <kcjackkelly@gmail.com>
2022-07-24 19:57:00 -0500

    shared form utils

---
+++ shared/utils/form.ts
@@ -0,0 +1,21 @@
+import Busboy from 'busboy';
+import { NextApiRequest } from 'next';
+import { NextkitError } from 'nextkit';
+
+export const populateFormData = (data: Record<string, unknown>) => {
+	const formData = new FormData();
+
+	Object.entries(data).forEach(([key, value]) => {
+		if (value instanceof File) {
+			formData.append(key, value, value.name);
+		} else if (value instanceof Date) {
+			formData.append(key, value.toISOString());
+		} else {
+			if (value !== undefined && !(typeof value === 'string' && value?.length === 0)) {
+				formData.append(key, String(value));
+			}
+		}
+	});
+
+	return formData;
+};
