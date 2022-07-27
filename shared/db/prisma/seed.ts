fb3f2ba2643074a453f277445d17503832ddde8e
Jack Kelly <kcjackkelly@gmail.com>
2022-07-26 20:07:00 -0500

    use prisma client edge for seeding

--- shared/db/prisma/seed.ts
+++ shared/db/prisma/seed.ts
@@ -1,4 +1,4 @@
-import { PrismaClient } from '@prisma/client';
+import { PrismaClient } from '@prisma/client/edge';
 
 import { SEED_USERS_TO_GENERATE } from '../../utils';
 import { fake } from './fake';
